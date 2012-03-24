e91e7127742cc5eef5803ac119cca8e4afa04063
Benson Peng <penglingzhe@gmail.com>
2012-03-24 20:41:51 +0800

    add change avatar

---
+++ Draw/Controller/User/ChangeAvatar.h
@@ -0,0 +1,13 @@
+//
+//  ChangeAvatar.h
+//  Draw
+//
+//  Created by  on 12-3-24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChangeAvatar : NSObject
+
+@end
---
+++ Draw/Controller/User/ChangeAvatar.m
@@ -0,0 +1,13 @@
+//
+//  ChangeAvatar.m
+//  Draw
+//
+//  Created by  on 12-3-24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ChangeAvatar.h"
+
+@implementation ChangeAvatar
+
+@end
