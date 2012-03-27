3e1b8dab9d586cf9608ce74290c268637dbd245e
Benson Peng <penglingzhe@gmail.com>
2012-03-27 14:36:09 +0800

    add account service

---
+++ Draw/Service/User/AccountService.h
@@ -0,0 +1,13 @@
+//
+//  AccountService.h
+//  Draw
+//
+//  Created by  on 12-3-27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "CommonService.h"
+
+@interface AccountService : CommonService
+
+@end
