e37dcca13434bbff46aaaee060d98b9f22f6ab50
kira <ouyongyong@163.com>
2012-03-29 17:33:28 +0800

    add ShareEditController

---
+++ Draw/Controller/Share/ShareEditController.m
@@ -0,0 +1,44 @@
+//
+//  ShareEditController.m
+//  Draw
+//
+//  Created by Orange on 12-3-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ShareEditController.h"
+
+@interface ShareEditController ()
+
+@end
+
+@implementation ShareEditController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
