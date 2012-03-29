bf9d0fa903c41c6673a1c09faa6e7351ee967225
Benson Peng <penglingzhe@gmail.com>
2012-03-29 17:09:40 +0800

    add my paint objects

---
+++ Draw/Model/MyPaint/MyPaintManager.h
@@ -0,0 +1,13 @@
+//
+//  MyPaintManager.h
+//  Draw
+//
+//  Created by  on 12-3-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface MyPaintManager : CommonManager
+
+@end
---
+++ Draw/Model/MyPaint/MyPaintManager.m
@@ -0,0 +1,13 @@
+//
+//  MyPaintManager.m
+//  Draw
+//
+//  Created by  on 12-3-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "MyPaintManager.h"
+
+@implementation MyPaintManager
+
+@end
