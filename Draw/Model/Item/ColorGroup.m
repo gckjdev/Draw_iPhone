4409adb04810b9c54cd758813e32638687fa4aa9
GamyHuang <hguangm2009@gmail.com>
2012-04-03 16:50:53 +0800

    add files

---
+++ Draw/Model/Item/ColorGroup.m
@@ -0,0 +1,13 @@
+//
+//  ColorGroup.m
+//  Draw
+//
+//  Created by  on 12-4-3.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ColorGroup.h"
+
+@implementation ColorGroup
+
+@end
