2eeedbde2f713e9c1a457bc3d3628c7c6b831d7f
Benson Peng <penglingzhe@gmail.com>
2012-04-04 13:28:08 +0800

    add item manager

---
+++ Draw/Model/Item/UserItem.m
@@ -0,0 +1,17 @@
+//
+//  UserItem.m
+//  Draw
+//
+//  Created by  on 12-4-4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "UserItem.h"
+
+
+@implementation UserItem
+
+@dynamic itemType;
+@dynamic amount;
+
+@end
