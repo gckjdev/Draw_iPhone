448ef93b19f5f65c27563f18368255dbc05ead4d
GamyHuang <hguangm2009@gmail.com>
2012-04-24 14:33:44 +0800

    add files and adjust the view directory.

---
+++ Draw/View/ShopView/ColorShopView.h
@@ -0,0 +1,13 @@
+//
+//  ColorShopView.h
+//  Draw
+//
+//  Created by  on 12-4-24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ColorShopView : UIViewController
+
+@end
