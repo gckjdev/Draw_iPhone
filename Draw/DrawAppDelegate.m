2ad78ebf194ccca124956d533a95a3b5b63eae39
Benson Peng <penglingzhe@gmail.com>
2012-04-30 22:23:09 +0800

    init iPad support

--- Draw/DrawAppDelegate.m
+++ Draw/DrawAppDelegate.m
@@ -30,7 +30,7 @@
 NSString* GlobalGetServerURL()
 {    
     NSString* url = [MobClick getConfigParams:@"API_SERVER_URL"];
-    return (url == nil) ? @"http://you100.me:8001/api/i?" : url;
+    return (url == nil) ? @"http://58.215.189.146:8001/api/i?" : url;
     
 //    return @"http://you100.me:8001/api/i?";        
 //    return @"http://106.187.89.232:8001/api/i?";    
