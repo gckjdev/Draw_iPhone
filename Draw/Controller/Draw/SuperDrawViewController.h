6fb1c26f3a0d9a579e60f4d95c1f46f9a628119c
GamyHuang <hguangm2009@gmail.com>
2012-05-03 11:13:41 +0800

    add super draw view controller.

---
+++ Draw/Controller/Draw/SuperDrawViewController.h
@@ -0,0 +1,15 @@
+//
+//  SuperDrawViewController.h
+//  Draw
+//
+//  Created by  on 12-5-3.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PPViewController.h"
+@interface SuperDrawViewController : UIViewController
+{
+    
+}
+@end
