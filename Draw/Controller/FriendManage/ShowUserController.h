fcccc5d972f0e1c383ce4001ea7f64fac943b53a
haodong <qiuhaodong2006@126.com>
2012-05-08 17:04:45 +0800

    update FriendManager

---
+++ Draw/Controller/FriendManage/ShowUserController.h
@@ -0,0 +1,13 @@
+//
+//  ShowUserController.h
+//  Draw
+//
+//  Created by haodong qiu on 12年5月8日.
+//  Copyright (c) 2012年 orange. All rights reserved.
+//
+
+#import "PPViewController.h"
+
+@interface ShowUserController : PPViewController
+
+@end
--- Draw/Model/Friend/FriendManager.m
+++ Draw/Model/Friend/FriendManager.m
@@ -9,10 +9,26 @@
 #import "FriendManager.h"
 #import "Friend.h"
 #import "CoreDataUtil.h"
+#import "LogUtil.h"
+
+enum deleteFlag {
+    notDeleted = 0,
+    isDeleted = 1
+};
 
 @implementation FriendManager
 
-- (void)createFriendWithUserId:(NSString *)friendUserId 
+static FriendManager *_defaultFriendManager = nil;
+
++ (FriendManager *)defaultManager
+{
+    if (_defaultFriendManager == nil) {
+        _defaultFriendManager = [[FriendManager alloc] init];
+    }
+    return _defaultFriendManager;
+}
+
+- (BOOL)createFriendWithUserId:(NSString *)friendUserId 
                       nickName:(NSString *)nickName 
                         avatar:(NSString *)avatar 
                         gender:(NSString *)gender
@@ -25,7 +41,46 @@
                           type:(NSNumber *)type
                   onlineStatus:(NSNumber *)onlineStatus
 {
+    CoreDataManager *dataManager = [CoreDataManager defaultManager];
+    Friend *newFriend = [dataManager insert:@"Friend"];
+    [newFriend setFriendUserId:friendUserId];
+    [newFriend setNickName:nickName];
+    [newFriend setAvatar:avatar];
+    [newFriend setGender:gender];
+    [newFriend setSinaId:sinaId];
+    [newFriend setQqId:qqId];
+    [newFriend setFacebookId:facebookId];
+    [newFriend setSinaNick:sinaNick];
+    [newFriend setQqNick:qqNick];
+    [newFriend setFacebookNick:facebookNick];
+    [newFriend setType:type];
+    [newFriend setOnlineStatus:onlineStatus];
+    [newFriend setCreateDate:[NSDate date]];
+    [newFriend setLastModifiedDate:[NSData data]];
+    [newFriend setDeleteFlag:[NSNumber numberWithInt:notDeleted]];
     
+    PPDebug(@"<createFriendWithUserId> %@", [newFriend description]);
+    return [dataManager save];
+}
+
+- (NSArray *)findAllFanFriends
+{
+    CoreDataManager *dataManager = [CoreDataManager defaultManager];
+    return [dataManager execute:@"findAllFanFriends" sortBy:@"createDate" ascending:NO];
+}
+
+- (NSArray *)findAllFollowFriends
+{
+    CoreDataManager *dataManager = [CoreDataManager defaultManager];
+    return [dataManager execute:@"findAllFollowFriends" sortBy:@"createDate" ascending:NO];
+}
+
+- (BOOL)deleteFollowFriend:(NSString *)friendUserId
+{
+    CoreDataManager *dataManager = [CoreDataManager defaultManager];
+    Friend *friend = (Friend*)[dataManager execute:@"findFollowByFriendUserId" forKey:@"FRIEND_USER_ID" value:friendUserId];
+    [friend setDeleteFlag:[NSNumber numberWithInt:isDeleted]];
+    return [dataManager save];
 }
 
 @end
