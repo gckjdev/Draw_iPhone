59071eecc57d2daf130e2d4182982fff0b55bac0
GamyHuang <hguangm2009@gmail.com>
2012-05-11 10:29:48 +0800

    add fiend room files.

---
+++ Draw/Controller/Room/FriendRoomController.h
@@ -0,0 +1,13 @@
+//
+//  FriendRoomController.h
+//  Draw
+//
+//  Created by  on 12-5-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendRoomController : UIViewController
+
+@end
