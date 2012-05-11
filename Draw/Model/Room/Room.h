3681118bb596fc25a409d5d93681b8a9bd87956b
GamyHuang <hguangm2009@gmail.com>
2012-05-11 10:58:20 +0800

    add room model and service.

---
+++ Draw/Model/Room/Room.h
@@ -0,0 +1,13 @@
+//
+//  Room.h
+//  Draw
+//
+//  Created by  on 12-5-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Room : NSObject
+
+@end
---
+++ Draw/Model/Room/Room.m
@@ -0,0 +1,13 @@
+//
+//  Room.m
+//  Draw
+//
+//  Created by  on 12-5-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "Room.h"
+
+@implementation Room
+
+@end
