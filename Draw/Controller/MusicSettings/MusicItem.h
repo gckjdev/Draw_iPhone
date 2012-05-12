467092bb2830f868c3a895dd3586a5977d4cb2b7
kaibin <ekaibwu@gmail.com>
2012-05-12 13:53:52 +0800

    add MusicSetting File

---
+++ Draw/Controller/MusicSettings/MusicItem.h
@@ -0,0 +1,13 @@
+//
+//  MusicItem.h
+//  Draw
+//
+//  Created by gckj on 12-5-12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MusicItem : NSObject
+
+@end
