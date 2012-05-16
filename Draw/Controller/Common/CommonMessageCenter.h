03ddf523e5e2d2a9fb4b672598f9cb414b67fe86
kira <ouyongyong@163.com>
2012-05-16 16:27:50 +0800

    add common message view

---
+++ Draw/Controller/Common/CommonMessageCenter.h
@@ -0,0 +1,13 @@
+//
+//  CommonMessageCenter.h
+//  Draw
+//
+//  Created by Orange on 12-5-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CommonMessageCenter : NSObject
+
+@end
---
+++ Draw/Controller/Common/CommonMessageCenter.m
@@ -0,0 +1,13 @@
+//
+//  CommonMessageCenter.m
+//  Draw
+//
+//  Created by Orange on 12-5-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "CommonMessageCenter.h"
+
+@implementation CommonMessageCenter
+
+@end
