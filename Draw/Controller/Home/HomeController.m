f49368aa0592ae25c2f66b2395bd3918e7e25a44
Benson Peng <penglingzhe@gmail.com>
2012-05-28 12:57:42 +0800

    refactor API Server URL

--- Draw/Controller/Home/HomeController.m
+++ Draw/Controller/Home/HomeController.m
@@ -365,7 +365,7 @@
     [[DrawGameService defaultService] setServerAddress:address];
     [[DrawGameService defaultService] setServerPort:port];    
     
-//    [[DrawGameService defaultService] setServerAddress:@"192.168.1.101"];
+//    [[DrawGameService defaultService] setServerAddress:@"192.168.1.198"];
 //    [[DrawGameService defaultService] setServerPort:8080];   
 
 //    [[DrawGameService defaultService] setServerAddress:@"58.215.188.215"];
