c8ed52941aed2a13c0ab393fadc6806e83b25157
Benson Peng <penglingzhe@gmail.com>
2012-05-30 17:47:47 +0800

    2.2 submission

--- Draw/Controller/Draw/DrawViewController.m
+++ Draw/Controller/Draw/DrawViewController.m
@@ -127,14 +127,15 @@
 
     [colorViewArray addObject:[ColorView blackColorView]];
     [colorViewArray addObject:[ColorView redColorView]];
-    [colorViewArray addObject:[ColorView greenColorView]];
-    [colorViewArray addObject:[ColorView blueColorView]];
     [colorViewArray addObject:[ColorView yellowColorView]];
-    [colorViewArray addObject:[ColorView orangeColorView]];
-    [colorViewArray addObject:[ColorView pinkColorView]];
-    [colorViewArray addObject:[ColorView brownColorView]];
-    [colorViewArray addObject:[ColorView skyColorView]];
-//    [colorViewArray addObject:[ColorView whiteColorView]];
+    [colorViewArray addObject:[ColorView blueColorView]];
+    [colorViewArray addObject:[ColorView whiteColorView]];
+
+//    [colorViewArray addObject:[ColorView orangeColorView]];
+//    [colorViewArray addObject:[ColorView greenColorView]];
+//    [colorViewArray addObject:[ColorView pinkColorView]];
+//    [colorViewArray addObject:[ColorView brownColorView]];
+//    [colorViewArray addObject:[ColorView skyColorView]];
         
     [pickPenView setColorViews:colorViewArray];
     [colorViewArray release];
