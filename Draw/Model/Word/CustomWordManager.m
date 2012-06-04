44ac2af8739051bdbeda9a902c1bebb48a7e41a9
haodong <qiuhaodong2006@126.com>
2012-06-04 15:14:12 +0800

    add CustomWord

---
+++ Draw/Model/Word/CustomWordManager.m
@@ -0,0 +1,13 @@
+//
+//  CustomWordManager.m
+//  Draw
+//
+//  Created by haodong qiu on 12年6月4日.
+//  Copyright (c) 2012年 orange. All rights reserved.
+//
+
+#import "CustomWordManager.h"
+
+@implementation CustomWordManager
+
+@end
