66c67ea49e183e313616442f54bb96303724bd49
GamyHuang <hguangm2009@gmail.com>
2012-06-04 17:16:16 +0800

    update files.

--- Draw/Controller/Draw/Offline/OfflineDrawViewController.h
+++ Draw/Controller/Draw/Offline/OfflineDrawViewController.h
@@ -1,13 +1,57 @@
 //
-//  OfflineDrawViewController.h
+//  DrawViewController.h
 //  Draw
 //
-//  Created by  on 12-6-4.
-//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//  Created by gamy on 12-3-4.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface OfflineDrawViewController : UIViewController
+#import "DrawView.h"
+#import "DrawGameService.h"
+#import "PickView.h"
+#import "CommonDialog.h"
+#import "SuperGameViewController.h"
+#import "ColorShopView.h"
+
+@class Word;
+@class ShareImageManager;
+@class ColorShopController;
+@class PenView;
+@class PickPenView;
+@class PickEraserView;
+@class PickColorView;
+
+@interface OnlineDrawViewController : SuperGameViewController<DrawViewDelegate,PickViewDelegate,CommonDialogDelegate,ColorShopViewDelegate> {
+    DrawView *drawView;
+    PickColorView *pickColorView;
+    PickEraserView *pickEraserView;
+    PickPenView *pickPenView;
+    NSInteger penWidth;
+    NSInteger eraserWidth;
+    PenView *_willBuyPen;
+}
+
+- (IBAction)clickChangeRoomButton:(id)sender;
+- (IBAction)clickRedraw:(id)sender;
+- (IBAction)clickEraserButton:(id)sender;
+- (IBAction)clickPenButton:(id)sender;
+- (IBAction)clickColorButton:(id)sender;
+
+@property (retain, nonatomic) IBOutlet UIButton *eraserButton;
+@property (retain, nonatomic) IBOutlet UIButton *wordButton;
+@property (retain, nonatomic) IBOutlet UIButton *cleanButton;
+@property (retain, nonatomic) IBOutlet PenView *penButton;
+@property (retain, nonatomic) IBOutlet ColorView *colorButton;
+@property (retain, nonatomic) GameMessage *gameCompleteMessage;
+
++ (void)startDraw:(Word *)word fromController:(UIViewController*)fromController;
+
+- (id)initWithWord:(Word *)word lang:(LanguageType)lang;
+- (void)initEraser;
+- (void)initPens;
+- (void)initDrawView;
 
 @end
+
--- Draw/Controller/Draw/Offline/OfflineGuessDrawController.h
+++ Draw/Controller/Draw/Offline/OfflineGuessDrawController.h
@@ -1,13 +1,64 @@
 //
-//  OfflineGuessDrawController.h
+//  ShowDrawController.h
 //  Draw
 //
-//  Created by  on 12-6-4.
+//  Created by  on 12-3-19.
 //  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "DrawGameService.h"
+#import "UserManager.h"
+#import "CommonDialog.h"
+#import "SuperGameViewController.h"
 
-@interface OfflineGuessDrawController : UIViewController
+
+@class Word;
+@class ShowDrawView;
+@class ShareImageManager;
+@class ToolView;
+@class ItemShopController;
+@interface OnlineGuessDrawController : SuperGameViewController<CommonDialogDelegate,UIScrollViewDelegate>
+{
+    ShowDrawView *showView;
+    NSString *_candidateString;
+    ItemShopController *_shopController;
+    ToolView *toolView;
+    BOOL _guessCorrect;
+    UIButton *moveButton;
+    UIButton *lastScaleTarget;
+    
+    NSInteger numberPerPage;
+    NSInteger pageCount;
+}
+@property (retain, nonatomic) NSString *candidateString;
+@property (retain, nonatomic) ShowDrawView *showView;
+
+@property (retain, nonatomic) IBOutlet UIImageView *drawBackground;
+@property (retain, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (retain, nonatomic) IBOutlet UIPageControl *pageControl;
+@property (retain, nonatomic) IBOutlet UIButton *leftPageButton;
+@property (retain, nonatomic) IBOutlet UIButton *rightPageButton;
+
+- (IBAction)clickRunAway:(id)sender;
+- (void)bomb:(id)sender;
+- (void)commitAnswer:(NSString *)answer;
+- (IBAction)clickLeftPage:(id)sender;
+- (IBAction)clickRightPage:(id)sender;
+
+- (void)setButton:(UIButton *)button title:(NSString *)title enabled:(BOOL)enabled;
+- (NSString *)realValueForButton:(UIButton *)button;
+- (void)initShowView;
+- (void)initBomb;
+
+- (void)initTargetViews;
+- (void)updateCandidateViews:(Word *)word lang:(LanguageType)lang;
+- (void)updateTargetViews:(Word *)word;
+- (void)initWithCachData;
+
+- (void)scrollToPage:(NSInteger)pageIndex;
+
 
 @end
+
+
