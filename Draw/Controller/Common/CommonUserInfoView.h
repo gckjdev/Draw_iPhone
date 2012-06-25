66d75bfb3d954ad5ae64702f6b94af68da6ad4cf
kira <ouyongyong@163.com>
2012-06-25 09:54:21 +0800

    add common user info view

---
+++ Draw/Controller/Common/CommonUserInfoView.h
@@ -0,0 +1,13 @@
+//
+//  CommonUserInfoView.h
+//  Draw
+//
+//  Created by Orange on 12-6-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommonUserInfoView : UIView
+
+@end
---
+++ Draw/Controller/Common/CommonUserInfoView.m
@@ -0,0 +1,31 @@
+//
+//  CommonUserInfoView.m
+//  Draw
+//
+//  Created by Orange on 12-6-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "CommonUserInfoView.h"
+
+@implementation CommonUserInfoView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
