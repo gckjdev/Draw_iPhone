a52fdeb38ae8c200d56de0ea47c9de7c1ccf76a9
GamyHuang <hguangm2009@gmail.com>
2012-06-25 15:10:05 +0800

    fix bugs.

--- Draw/Controller/Feed/FeedDetailController.m
+++ Draw/Controller/Feed/FeedDetailController.m
@@ -105,17 +105,15 @@
 
 - (void)updateGuessDesc:(Feed *)feed
 {
-    if (feed.matchTimes == 0) {
+    if (feed.guessTimes == 0) {
         [self.guessStatLabel setText:NSLS(@"kNoGuess")];
     }else{
-        NSInteger guessTimes = feed.matchTimes;
+        NSInteger guessTimes = feed.guessTimes;
         NSInteger correctTimes = feed.correctTimes;
         NSString *desc = [NSString stringWithFormat:NSLS(@"kGuessStat"),guessTimes, correctTimes];
         [self.guessStatLabel setText:desc];        
     }
 }
-
-
 
 
 - (void)updateActionButton:(Feed *)feed
