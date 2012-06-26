c9b3b7581c947298f73659cfbb1e2807c01fd3e1
Benson Peng <penglingzhe@gmail.com>
2012-06-27 00:25:15 +0800

    2.9 version 91 submission

--- Draw/Controller/Home/HomeController.m
+++ Draw/Controller/Home/HomeController.m
@@ -467,11 +467,11 @@
         port = [server.port intValue];            
     }
 
-//    [[DrawGameService defaultService] setServerAddress:address];
-//    [[DrawGameService defaultService] setServerPort:port];    
+    [[DrawGameService defaultService] setServerAddress:address];
+    [[DrawGameService defaultService] setServerPort:port];    
 
-    [[DrawGameService defaultService] setServerAddress:@"192.168.1.101"];
-    [[DrawGameService defaultService] setServerPort:8080];   
+//    [[DrawGameService defaultService] setServerAddress:@"192.168.1.101"];
+//    [[DrawGameService defaultService] setServerPort:8080];   
 
 //    [[DrawGameService defaultService] setServerAddress:@"192.168.1.198"];
 //    [[DrawGameService defaultService] setServerPort:8080];   
