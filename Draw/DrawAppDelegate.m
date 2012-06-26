dfb90f659764db5aa602e107f6bc42f1e508341a
Benson Peng <penglingzhe@gmail.com>
2012-06-26 17:56:47 +0800

    update

--- Draw/DrawAppDelegate.m
+++ Draw/DrawAppDelegate.m
@@ -42,7 +42,7 @@
 NSString* GlobalGetServerURL()
 {    
 //    return [ConfigManager getAPIServerURL];
-      return @"http://192.168.1.12:8000/api/i?";  
+      return @"http://192.168.1.101:8000/api/i?";  
 //    return @"http://you100.me:8001/api/i?";        
 //    return @"http://106.187.89.232:8001/api/i?";    
 //    return @"http://192.168.1.9:8000/api/i?";    
@@ -51,7 +51,7 @@ NSString* GlobalGetServerURL()
 NSString* GlobalGetTrafficServerURL()
 {
 //    return [ConfigManager getTrafficAPIServerURL];
-    return @"http://192.168.1.12:8100/api/i?";    
+    return @"http://192.168.1.101:8100/api/i?";    
 }
 
 @implementation DrawAppDelegate
