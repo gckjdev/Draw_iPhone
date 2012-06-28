e86fcbadbab282de910f9e2679e1f22cc6fbc940
Benson Peng <penglingzhe@gmail.com>
2012-06-28 11:47:04 +0800

    add ShareService

---
+++ Draw/Service/Share/ShareService.h
@@ -0,0 +1,13 @@
+//
+//  ShareService.h
+//  Draw
+//
+//  Created by  on 12-6-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShareService : NSObject
+
+@end
---
+++ Draw/Service/Share/ShareService.m
@@ -0,0 +1,13 @@
+//
+//  ShareService.m
+//  Draw
+//
+//  Created by  on 12-6-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ShareService.h"
+
+@implementation ShareService
+
+@end
