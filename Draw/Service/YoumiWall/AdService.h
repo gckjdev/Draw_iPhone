46beeaa1f426941525a666bdadbf33a8378fe4f8
Benson Peng <penglingzhe@gmail.com>
2012-06-29 17:23:29 +0800

    add AdService

---
+++ Draw/Service/YoumiWall/AdService.h
@@ -0,0 +1,13 @@
+//
+//  AdService.h
+//  Draw
+//
+//  Created by  on 12-6-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AdService : NSObject
+
+@end
---
+++ Draw/Service/YoumiWall/AdService.m
@@ -0,0 +1,13 @@
+//
+//  AdService.m
+//  Draw
+//
+//  Created by  on 12-6-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "AdService.h"
+
+@implementation AdService
+
+@end
