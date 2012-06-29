d1c3d56077d417db6106675ef721542a18d5d4e2
Benson Peng <penglingzhe@gmail.com>
2012-06-29 10:37:29 +0800

    add LmWallService

---
+++ Draw/Service/YoumiWall/LmWallService.h
@@ -0,0 +1,13 @@
+//
+//  LmWallService.h
+//  Draw
+//
+//  Created by  on 12-6-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LmWallService : NSObject
+
+@end
---
+++ Draw/Service/YoumiWall/LmWallService.m
@@ -0,0 +1,13 @@
+//
+//  LmWallService.m
+//  Draw
+//
+//  Created by  on 12-6-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "LmWallService.h"
+
+@implementation LmWallService
+
+@end
