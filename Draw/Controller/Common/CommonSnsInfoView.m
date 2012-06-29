e260dbbe63ac2cb8dd3b7fd62caf5e8bdb556626
kira <ouyongyong@163.com>
2012-06-29 10:37:22 +0800

    add common sns info view

---
+++ Draw/Controller/Common/CommonSnsInfoView.m
@@ -0,0 +1,31 @@
+//
+//  CommonSnsInfoView.m
+//  Draw
+//
+//  Created by Orange on 12-6-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "CommonSnsInfoView.h"
+
+@implementation CommonSnsInfoView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
