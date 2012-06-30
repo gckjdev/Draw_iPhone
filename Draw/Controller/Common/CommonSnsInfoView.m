ad1ac9a2ffab889bad844f2a3ab7eb123136f67b
kira <ouyongyong@163.com>
2012-06-30 15:45:13 +0800

    fix bug

--- Draw/Controller/Common/CommonSnsInfoView.m
+++ Draw/Controller/Common/CommonSnsInfoView.m
@@ -36,7 +36,6 @@
             UIView* view = [snsImageArray objectAtIndex:i];
             [view setFrame:CGRectMake(i*frame.size.height, 0, aFrame.size.height, aFrame.size.height)];
             [self addSubview:view];
-            [view release];
         }
         
     }
