d2bacc0c1d1d74d6fcc2304b2469238fae1ca729
kira <ouyongyong@163.com>
2012-07-09 14:27:02 +0800

    add vending controller

---
+++ Draw/Controller/Shop/VendingController.h
@@ -0,0 +1,13 @@
+//
+//  VendingController.h
+//  Draw
+//
+//  Created by Orange on 12-7-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "PPViewController.h"
+
+@interface VendingController : PPViewController
+
+@end
