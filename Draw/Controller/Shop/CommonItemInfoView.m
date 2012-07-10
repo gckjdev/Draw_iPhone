1b041a6245c10aa81a480a5795c78b221baea15a
kira <ouyongyong@163.com>
2012-07-10 14:28:53 +0800

    add item info view

---
+++ Draw/Controller/Shop/CommonItemInfoView.m
@@ -0,0 +1,31 @@
+//
+//  CommonItemInfoView.m
+//  Draw
+//
+//  Created by Orange on 12-7-10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "CommonItemInfoView.h"
+
+@implementation CommonItemInfoView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
