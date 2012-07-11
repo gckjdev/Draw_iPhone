8baa3436e3d5734c6ba717a8d56f0a57ea4fa038
GamyHuang <hguangm2009@gmail.com>
2012-07-11 13:09:47 +0800

    update pick view.

---
+++ Draw/View/PickView/PickToolView.m
@@ -0,0 +1,23 @@
+//
+//  PickToolView.m
+//  Draw
+//
+//  Created by  on 12-7-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "PickToolView.h"
+
+@implementation PickToolView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+@end
