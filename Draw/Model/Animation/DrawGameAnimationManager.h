970434360f356237906431be1a9c0b77620c7b27
kira <ouyongyong@163.com>
2012-07-13 16:57:01 +0800

    add animation manager

---
+++ Draw/Model/Animation/DrawGameAnimationManager.h
@@ -0,0 +1,13 @@
+//
+//  DrawGameAnimationManager.h
+//  Draw
+//
+//  Created by Orange on 12-7-13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DrawGameAnimationManager : NSObject
+
+@end
---
+++ Draw/Model/Animation/DrawGameAnimationManager.m
@@ -0,0 +1,13 @@
+//
+//  DrawGameAnimationManager.m
+//  Draw
+//
+//  Created by Orange on 12-7-13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "DrawGameAnimationManager.h"
+
+@implementation DrawGameAnimationManager
+
+@end
