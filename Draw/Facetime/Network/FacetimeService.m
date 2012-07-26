ea34e51986a66f1d72f2bb8e3a10dfac150207b3
Benson Peng <penglingzhe@gmail.com>
2012-07-26 09:51:44 +0800

    add face time files

---
+++ Draw/Facetime/Network/FacetimeService.m
@@ -0,0 +1,13 @@
+//
+//  FacetimeService.m
+//  Draw
+//
+//  Created by  on 12-7-26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "FacetimeService.h"
+
+@implementation FacetimeService
+
+@end
