857e9f2104374654b96c7fc4f2827be13c3e3b94
kira <ouyongyong@163.com>
2012-07-27 11:15:40 +0800

    add files

---
+++ Draw/Facetime/View/FacetimeUserInfoView.h
@@ -0,0 +1,13 @@
+//
+//  FacetimeUserInfoView.h
+//  Draw
+//
+//  Created by Orange on 12-7-27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FacetimeUserInfoView : UIView
+
+@end
