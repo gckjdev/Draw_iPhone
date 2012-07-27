2237e6ff315856597222eece1e61f24571938292
xiaotaowang <445242970@qq.com>
2012-07-27 16:54:01 +0800

    add controllers

---
+++ Draw/Dice/Controller/Room/DiceRoomListCell.h
@@ -0,0 +1,14 @@
+//
+//  RoomListCell.h
+//  Draw
+//
+//  Created by 小涛 王 on 12-7-27.
+//  Copyright (c) 2012年 甘橙软件. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PPTableViewCell.h"
+
+@interface DiceRoomListCell : PPTableViewCell
+
+@end
