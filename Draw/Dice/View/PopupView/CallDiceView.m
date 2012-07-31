9c28e4344c442302037b0ff3187f6ecd49b4a2c6
xiaotaowang <445242970@qq.com>
2012-07-31 15:26:30 +0800

    add font.

--- Draw/Dice/View/PopupView/CallDiceView.m
+++ Draw/Dice/View/PopupView/CallDiceView.m
@@ -9,6 +9,10 @@
 #import "CallDiceView.h"
 #import "DiceView.h"
 #import "LocaleUtils.h"
+#import "UIViewUtils.h"
+
+#define DICE_VIEW_TAG 101
+#define COUNT_LABEL_TAG 102
 
 #define CALL_DICE_VIEW_WIDTH 63
 #define CALL_DICE_VIEW_HEIGHT 35
@@ -26,21 +30,26 @@
         // Initialization code
         UIView *diceView = [[[DiceView alloc] initWithFrame:CGRectMake(36, 0, DICE_WIDTH, DICE_HEIGHT)   
                                                        dice:dice] autorelease];
+        diceView.tag = DICE_VIEW_TAG;
         diceView.userInteractionEnabled = NO;
         
         
-        UILabel *label = [[[UILabel alloc] initWithFrame:CGRectMake(0, 0, CALL_DICE_VIEW_WIDTH - DICE_WIDTH, CALL_DICE_VIEW_HEIGHT)] autorelease];
-        label.backgroundColor = [UIColor clearColor];
-        label.text = [NSString stringWithFormat:NSLS(@"%d X "), count]; 
+        UILabel *countLabel = [[[UILabel alloc] initWithFrame:CGRectMake(0, 0, 15, CALL_DICE_VIEW_HEIGHT)] autorelease];
+        countLabel.tag = COUNT_LABEL_TAG;
+        countLabel.backgroundColor = [UIColor clearColor];
+        countLabel.text = [NSString stringWithFormat:NSLS(@"%d"), count]; 
+        countLabel.font = [UIFont systemFontOfSize:27];
         
+        UILabel *symbolLabel = [[[UILabel alloc] initWithFrame:CGRectMake(20, 0, 12, CALL_DICE_VIEW_HEIGHT)] autorelease];
+        symbolLabel.backgroundColor = [UIColor clearColor];
+        symbolLabel.text = [NSString stringWithFormat:NSLS(@"x")]; 
         
-        [self addSubview:label];
+        [self addSubview:countLabel];
+        [self addSubview:symbolLabel];
         [self addSubview:diceView];
     }
     
     return self;
 }
-
-
 
 @end
