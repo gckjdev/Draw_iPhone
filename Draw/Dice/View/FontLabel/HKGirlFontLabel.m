31a4a2e2c980e1f1e884f4e2137852198ff9761c
kira <ouyongyong@163.com>
2012-08-02 10:47:25 +0800

    add HKGirlFontLabel

---
+++ Draw/Dice/View/FontLabel/HKGirlFontLabel.m
@@ -0,0 +1,31 @@
+//
+//  HKGirlFontLabel.m
+//  Draw
+//
+//  Created by Orange on 12-8-2.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "HKGirlFontLabel.h"
+
+@implementation HKGirlFontLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
