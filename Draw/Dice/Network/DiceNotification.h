61b3ccc0326f32786e9d860c6b910f2a00158cb0
Benson Peng <penglingzhe@gmail.com>
2012-08-04 10:30:27 +0800

    add dice notification

---
+++ Draw/Dice/Network/DiceNotification.h
@@ -0,0 +1,14 @@
+//
+//  DiceNotification.h
+//  Draw
+//
+//  Created by  on 12-8-4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Draw_DiceNotification_h
+#define Draw_DiceNotification_h
+
+
+
+#endif
