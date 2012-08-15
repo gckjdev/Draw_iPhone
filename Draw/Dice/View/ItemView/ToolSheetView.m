cdd85ed152c747397659f5605810341487cd6531
haodong <qiuhaodong2006@126.com>
2012-08-15 17:43:51 +0800

    update ToolSheetView

--- Draw/Dice/View/ItemView/ToolSheetView.m
+++ Draw/Dice/View/ItemView/ToolSheetView.m
@@ -9,9 +9,12 @@
 #import "ToolSheetView.h"
 #import "DeviceDetection.h"
 #import "DiceImageManager.h"
+#import "FontButton.h"
+#import "DiceFontManager.h"
 
 @interface ToolSheetView()
-@property (retain, nonatomic) NSArray *buttonImageNameList;
+//@property (retain, nonatomic) NSArray *buttonImageNameList;
+@property (retain, nonatomic) NSArray *titleList;
 @property (retain, nonatomic) NSArray *countNumberList;
 @property (retain, nonatomic) CMPopTipView *popTipView;
 @end
@@ -19,40 +22,40 @@
 
 
 @implementation ToolSheetView
-@synthesize buttonImageNameList = _buttonImageNameList;
 @synthesize delegate = _delegate;
+@synthesize titleList = _titleList;
 @synthesize countNumberList = _countNumberList;
 @synthesize popTipView = _popTipView;
 
 - (void)dealloc
 {
-    [_buttonImageNameList release];
+    [_titleList release];
     [_countNumberList release];
     [_popTipView release];
     [super dealloc];
 }
 
 
-- (id)initWithImageNameList:(NSArray *)imageNameList 
+//- (id)initWithTitleList:(NSArray *)titleList 
+//        countNumberList:(NSArray *)countNumberList 
+//               delegate:(id<ToolSheetViewDelegate>)delegate
+//{
+//    self  = [super init];
+//    if (self) {
+//        self.titleList = titleList;
+//        self.countNumberList = countNumberList;
+//        self.delegate = delegate;
+//        [self clearContent];
+//        [self showContent];
+//    }
+//    return self;
+//}
+
+- (void)updateWithTitleList:(NSArray *)titleList 
             countNumberList:(NSArray *)countNumberList 
                    delegate:(id<ToolSheetViewDelegate>)delegate
 {
-    self  = [super init];
-    if (self) {
-        self.buttonImageNameList = imageNameList;
-        self.countNumberList = countNumberList;
-        self.delegate = delegate;
-        [self clearContent];
-        [self showContent];
-    }
-    return self;
-}
-
-- (void)updateWithImageNameList:(NSArray *)imageNameList 
-                countNumberList:(NSArray *)countNumberList 
-                       delegate:(id<ToolSheetViewDelegate>)delegate
-{
-    self.buttonImageNameList = imageNameList;
+    self.titleList = titleList;
     self.countNumberList = countNumberList;
     self.delegate = delegate;
     [self clearContent];
@@ -84,45 +87,6 @@
 #define WIDTH_TOOL_BUTTON       (([DeviceDetection isIPAD]) ? 64 : 32 )
 #define HEIGHT_SHARP            (([DeviceDetection isIPAD]) ? 12 : 6 )
 #define OFFSET_ANIMATION        (([DeviceDetection isIPAD]) ? 24 : 12 )
-//- (void)showInView:(UIView *)superView fromFottomPoint:(CGPoint)fromFottomPoint
-//{
-//    if (_backgroundImageView == nil) {
-//        self.backgroundImageView = [[[UIImageView alloc] initWithImage:[[DiceImageManager defaultManager] toolBackground]] autorelease];
-//        [_backgroundImageView setAlpha:1];
-//    }
-//    [self addSubview:_backgroundImageView];
-//    
-//    int index = 0;
-//    for (NSString *name in _buttonImageNameList) {
-//        CGFloat yStart = SPACE_BORDE_AND_BUTTON + (SPACE_BORDE_AND_BUTTON + WIDTH_TOOL_BUTTON ) * index;
-//        CGRect toolButtonFrame = CGRectMake(SPACE_BORDE_AND_BUTTON, yStart, WIDTH_TOOL_BUTTON, WIDTH_TOOL_BUTTON);
-//        NSInteger toolButtonTag = index;
-//        
-//        UIButton *tooButton = [self createToolButton:toolButtonFrame 
-//                                                 tag:toolButtonTag 
-//                                           imageName:name 
-//                                               count:[_countNumberList objectAtIndex:index]];
-//        [self addSubview:tooButton];
-//        index ++;
-//    }
-//    
-//    CGFloat width = 2 * SPACE_BORDE_AND_BUTTON + WIDTH_TOOL_BUTTON;
-//    CGFloat height = index * (SPACE_BORDE_AND_BUTTON + WIDTH_TOOL_BUTTON) + SPACE_BORDE_AND_BUTTON + HEIGHT_SHARP;
-//    CGFloat x = fromFottomPoint.x - 0.5 * width;
-//    CGFloat y = fromFottomPoint.y- height;
-//    
-//    self.frame = CGRectMake(x, y + OFFSET_ANIMATION , width, height);
-//    _backgroundImageView.frame = CGRectMake(0, 0, width, height);
-//    [superView addSubview:self];
-//    
-//    self.alpha = 0.5;
-//    [UIView beginAnimations:nil context:nil];
-//    [UIView setAnimationDuration:0.15];
-//    self.frame = CGRectMake(x, y, width, height);
-//    self.alpha = 1.0;
-//    [UIImageView commitAnimations];
-//}
-
 - (void)clearContent
 {
     [[self subviews] makeObjectsPerformSelector:@selector(removeFromSuperview)];
@@ -131,14 +95,14 @@
 - (void)showContent
 {
     int index = 0;
-    for (NSString *name in _buttonImageNameList) {
+    for (NSString *title in _titleList) {
         CGFloat yStart = (SPACE_BORDE_AND_BUTTON + WIDTH_TOOL_BUTTON ) * index;
         CGRect toolButtonFrame = CGRectMake(0, yStart, WIDTH_TOOL_BUTTON, WIDTH_TOOL_BUTTON);
         NSInteger toolButtonTag = index;
         
         UIButton *tooButton = [self createToolButton:toolButtonFrame 
                                                  tag:toolButtonTag 
-                                           imageName:name 
+                                               title:title 
                                                count:[_countNumberList objectAtIndex:index]];
         [self addSubview:tooButton];
         index ++;
@@ -151,18 +115,20 @@
 }
 
 
-#define WIDTH_COUNT_VIEW    (([DeviceDetection isIPAD]) ? 28 : 14 )
-#define FONT_COUNT_LABEL    (([DeviceDetection isIPAD]) ? 20 : 10 )
+#define WIDTH_COUNT_VIEW        (([DeviceDetection isIPAD]) ? 28 : 14 )
+#define FONT_SIZE_BUTTON        (([DeviceDetection isIPAD]) ? 36 : 18 )
+#define FONT_SIZE_COUNT_LABEL   (([DeviceDetection isIPAD]) ? 20 : 10 )
+#define Y_OFFSET_TITLE          (([DeviceDetection isIPAD]) ? -8 : -4 )
 - (UIButton *)createToolButton:(CGRect)frame 
                            tag:(NSInteger)tag 
-                     imageName:(NSString *)imageName 
+                         title:(NSString *)title 
                          count:(NSNumber *)count
 {
-    UIButton *buttonTemp = [[[UIButton alloc] initWithFrame:frame] autorelease];
-    
-    [buttonTemp setImage:[UIImage imageNamed:imageName] forState:UIControlStateNormal];
+    FontButton *buttonTemp = [[FontButton alloc] initWithFrame:frame fontName:[[DiceFontManager defaultManager] fontName] pointSize:FONT_SIZE_BUTTON];
+    [buttonTemp setBackgroundImage:[[DiceImageManager defaultManager] toolsItemBgImage] forState:UIControlStateNormal];
+    buttonTemp.fontLable.text = title;
+    buttonTemp.fontLable.frame = CGRectOffset(buttonTemp.fontLable.frame, 0, Y_OFFSET_TITLE);
     [buttonTemp addTarget:self action:@selector(clickToolButton:) forControlEvents:UIControlEventTouchUpInside];
-    
     
     UIImage *countImage = nil;
     if ([count intValue] > 0 ) {
@@ -175,7 +141,7 @@
     UILabel *countLabel = [[[UILabel alloc] initWithFrame:countImageView.frame] autorelease];
     countLabel.text = [count stringValue];
     countLabel.textColor = [UIColor whiteColor];
-    countLabel.font = [UIFont systemFontOfSize:FONT_COUNT_LABEL];
+    countLabel.font = [UIFont systemFontOfSize:FONT_SIZE_COUNT_LABEL];
     countLabel.backgroundColor = [UIColor clearColor];
     countLabel.textAlignment = UITextAlignmentCenter;
     [buttonTemp addSubview:countImageView];
