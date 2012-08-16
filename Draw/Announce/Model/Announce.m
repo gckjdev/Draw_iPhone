17283f0b60f1c516031e3809ef48b36dfd1fb44a
GamyHuang <hguangm2009@gmail.com>
2012-08-16 15:52:15 +0800

    update draft image.

--- Draw/Announce/Model/Announce.m
+++ Draw/Announce/Model/Announce.m
@@ -9,5 +9,30 @@
 #import "Announce.h"
 
 @implementation Announce
+@synthesize type = _type;
+@synthesize url = _url;
+@synthesize version = _version;
+
+- (id)initWithType:(AnnounceType)type 
+           version:(NSString *)version 
+               url:(NSString *)url
+{
+    self = [super init];
+    if (self) {
+        self.type = type;
+        self.version = version;
+        self.url = url;
+    }
+    return self;
+}
+- (Announce *)announceWithType:(AnnounceType)type 
+                       version:(NSString *)version 
+                           url:(NSString *)url
+{
+    return [[[Announce alloc] initWithType:type 
+                                   version:version 
+                                       url:url] autorelease];
+}
+
 
 @end
