31a512cf9254020a6828b291b859c73e651530a5
GamyHuang <hguangm2009@gmail.com>
2012-08-16 14:56:12 +0800

    add annouce componnent.

---
+++ Draw/Announce/Network/AnnounceNetwork.m
@@ -0,0 +1,13 @@
+//
+//  AnnounceNetwork.m
+//  Draw
+//
+//  Created by  on 12-8-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "AnnounceNetwork.h"
+
+@implementation AnnounceNetwork
+
+@end
