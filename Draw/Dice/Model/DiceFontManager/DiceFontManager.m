cf6c4fabea39dcf9c20d32893dc32f460bf032be
haodong <qiuhaodong2006@126.com>
2012-08-17 12:38:28 +0800

    add unzip diceFont.zip

--- Draw/Dice/Model/DiceFontManager/DiceFontManager.m
+++ Draw/Dice/Model/DiceFontManager/DiceFontManager.m
@@ -9,8 +9,13 @@
 #import "DiceFontManager.h"
 #import "FontManager.h"
 #import "DeviceDetection.h"
+#import "FileUtil.h"
+#import "LogUtil.h"
+#import "SSZipArchive.h"
 
-#define HKGIRL_FONT @"diceFont"
+#define FONT_DIR        @"font"
+#define FONT_NAME       @"diceFont"
+#define FONT_ZIP_NAME   @"diceFont.zip"
 
 static DiceFontManager *_defaultManager = nil;
 
@@ -23,6 +28,75 @@ static DiceFontManager *_defaultManager = nil;
 @implementation DiceFontManager
 
 @synthesize fontName = _fontName;
+
++ (NSString *)fontDir
+{
+    return [[FileUtil getAppHomeDir] stringByAppendingPathComponent:FONT_DIR];
+}
+
++ (NSString *)fontPath
+{
+    return [[DiceFontManager fontDir] stringByAppendingPathComponent:FONT_NAME];    
+}
+
++ (BOOL)isExistsFontFile
+{
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    return [fileManager fileExistsAtPath:[DiceFontManager fontPath]];
+}
+
++ (void)unZipFiles
+{
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    if (queue == NULL) {
+        queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+    }
+    
+    if (queue == NULL) {
+        PPDebug(@"error:<DiceFontManager> fail to get queue");
+        return;
+    }
+    
+    dispatch_async(queue, ^{
+        
+        if ([DiceFontManager isExistsFontFile]){
+            PPDebug(@"<DiceFontManager>: font file is exists");
+            return;
+        }
+            
+        
+        PPDebug(@"start to unzip the font package");
+        //creat dir
+        NSString *dir = [DiceFontManager fontDir];
+        BOOL flag = [FileUtil createDir:dir];
+        
+        if (!flag) {
+            PPDebug(@"<DiceFontManager>:unZipFiles fail, due to failing to creating dir");
+            return;
+        }
+        
+        PPDebug(@"font dir = %@", dir);
+        
+        //copy the zip file to the dir
+        flag = [FileUtil copyFileFromBundleToAppDir:FONT_ZIP_NAME appDir:dir overwrite:YES];
+        if (!flag) {
+            PPDebug(@"<DiceFontManager>:unZipFiles fail, due to failing to copy font zip package to distination dir");
+        }
+        
+        //unzip to dir
+        NSString *zipPath = [dir stringByAppendingPathComponent:FONT_ZIP_NAME];
+        PPDebug(@"distination path = %@", zipPath);
+        flag = [SSZipArchive unzipFileAtPath:zipPath toDestination:dir];
+        if (!flag) {
+            PPDebug(@"<DiceFontManager>:unZipFiles fail, due to failing to unzip package to distination dir");
+            return;            
+        }else {
+            PPDebug(@"<DiceFontManager>:unZipFiles successfully");
+        }
+        [FileUtil removeFile:zipPath];
+    });
+    
+}
 
 - (void)dealloc
 {
@@ -42,13 +116,16 @@ static DiceFontManager *_defaultManager = nil;
 - (id)init
 {
     if (self = [super init]) {
+        
 //        if ([DeviceDetection isOS5]) {
 //            self.fontName = HKGIRL_FONT;
 //            [[FontManager sharedManager] loadFont:_fontName];
 //        }else {
 //            self.fontName = @"";
 //        }
-        self.fontName = HKGIRL_FONT;
+        
+        [[FontManager sharedManager] loadFontFromPath:[DiceFontManager fontPath]];
+        self.fontName = FONT_NAME;
     }
     
     return self;
