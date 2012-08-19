3616d7da5861b93f42e9f0a8d0d0506a5b1ca190
GamyHuang <hguangm2009@gmail.com>
2012-08-19 11:14:22 +0800

    add jump manager.

---
+++ Draw/Board/Model/JumpManager.h
@@ -0,0 +1,13 @@
+//
+//  JumpManager.h
+//  Draw
+//
+//  Created by  on 12-8-19.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JumpManager : NSObject
+
+@end
---
+++ Draw/Board/Model/JumpManager.m
@@ -0,0 +1,13 @@
+//
+//  JumpManager.m
+//  Draw
+//
+//  Created by  on 12-8-19.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "JumpManager.h"
+
+@implementation JumpManager
+
+@end
