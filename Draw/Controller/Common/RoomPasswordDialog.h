2dbea006866b4c6f293b53adf1fd03b55c3dfdca
kira <ouyongyong@163.com>
2012-08-20 16:28:01 +0800

    update

--- Draw/Controller/Common/RoomPasswordDialog.h
+++ Draw/Controller/Common/RoomPasswordDialog.h
@@ -8,12 +8,19 @@
 
 #import "InputDialog.h"
 
+#define ROOM_PASSWORD_DIALOG    @"RoomPasswordDialog"
+#define DICE_ROOM_PASSWORD_DIALOG   @"DiceRoomPasswordDialog"
+
 @interface RoomPasswordDialog : InputDialog
 @property (retain, nonatomic) IBOutlet UITextField *passwordField;
 @property (retain, nonatomic) IBOutlet UILabel *roomNameLabel;
 @property (retain, nonatomic) IBOutlet UILabel *passwordLabel;
 
 
-+ (RoomPasswordDialog *)dialogWith:(NSString *)title delegate:(id<InputDialogDelegate>)delegate;
++ (RoomPasswordDialog *)dialogWith:(NSString *)title 
+                          delegate:(id<InputDialogDelegate>)delegate;
++ (RoomPasswordDialog *)dialogWith:(NSString *)title 
+                          delegate:(id<InputDialogDelegate>)delegate 
+                             theme:(CommonInputDialogTheme)theme;
 
 @end
