6107d8598abf7f1369f9900b9b23612aaa39dbac
GamyHuang <hguangm2009@gmail.com>
2012-08-20 17:59:06 +0800

    update the frame.

--- Draw/Controller/Share/MyPaintButton.m
+++ Draw/Controller/Share/MyPaintButton.m
@@ -68,18 +68,22 @@
     if (image == nil) {
         if (paint.drawThumbnailData == nil){
             NSString* imagePath = [MyPaintManager getMyPaintImagePathByCapacityPath:paint.image];
-            paint.thumbImage = [[[UIImage alloc] initWithContentsOfFile:imagePath] autorelease];
-            PPDebug(@"<MyPaintButton>: load image from path = %@", imagePath);
+            if (imagePath) {
+                PPDebug(@"<MyPaintButton>: load image from path = %@", imagePath);
+                paint.thumbImage = [[[UIImage alloc] initWithContentsOfFile:imagePath] autorelease];                
+            }
         }
         else{
             NSData *data = paint.drawThumbnailData;
-            CGImageSourceRef imageRef = CGImageSourceCreateWithData((CFDataRef)data, (CFDictionaryRef)dict);            
-            CGImageRef imageIndexRef = CGImageSourceCreateImageAtIndex(imageRef, 0, NULL);
-            UIImage* image = [UIImage imageWithCGImage:imageIndexRef];
-            paint.thumbImage = image;
-            CFRelease(imageIndexRef);
-            CFRelease(imageRef);
-            PPDebug(@"<MyPaintButton>: load image from thumbnail data");
+            if (data) {
+                CGImageSourceRef imageRef = CGImageSourceCreateWithData((CFDataRef)data, (CFDictionaryRef)dict);            
+                CGImageRef imageIndexRef = CGImageSourceCreateImageAtIndex(imageRef, 0, NULL);
+                UIImage* image = [UIImage imageWithCGImage:imageIndexRef];
+                paint.thumbImage = image;
+                CFRelease(imageIndexRef);
+                CFRelease(imageRef);
+                PPDebug(@"<MyPaintButton>: load image from thumbnail data");                
+            }
         }
     }
     [self.drawImage setImage:paint.thumbImage];
