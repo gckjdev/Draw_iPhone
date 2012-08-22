df12cab02d695987c54a5d38236b54fad8b00672
kira <ouyongyong@163.com>
2012-08-22 16:39:36 +0800

    add soundmanager

---
+++ Draw/Dice/Model/Sound/DiceSoundManager.h
@@ -0,0 +1,13 @@
+//
+//  DiceSoundManager.h
+//  Draw
+//
+//  Created by Orange on 12-8-22.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DiceSoundManager : NSObject
+
+@end
---
+++ Draw/Dice/Model/Sound/DiceSoundManager.m
@@ -0,0 +1,13 @@
+//
+//  DiceSoundManager.m
+//  Draw
+//
+//  Created by Orange on 12-8-22.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "DiceSoundManager.h"
+
+@implementation DiceSoundManager
+
+@end
