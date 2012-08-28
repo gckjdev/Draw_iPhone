e6a6f9b823991789f252566ee83ff32e7d5e6ea6
kira <ouyongyong@163.com>
2012-08-28 17:13:36 +0800

    add dice level service

---
+++ Draw/Dice/Service/DiceLevelService.h
@@ -0,0 +1,13 @@
+//
+//  DiceLevelService.h
+//  Draw
+//
+//  Created by Orange on 12-8-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "CommonService.h"
+
+@interface DiceLevelService : CommonService
+
+@end
---
+++ Draw/Dice/Service/DiceLevelService.m
@@ -0,0 +1,13 @@
+//
+//  DiceLevelService.m
+//  Draw
+//
+//  Created by Orange on 12-8-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "DiceLevelService.h"
+
+@implementation DiceLevelService
+
+@end
