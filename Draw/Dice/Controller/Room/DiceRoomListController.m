db002f0bbc2f4acbe2e623af0110ad736fc5037b
haodong <qiuhaodong2006@126.com>
2012-08-29 17:15:17 +0800

    update DiceHelpView for english

--- Draw/Dice/Controller/Room/DiceRoomListController.m
+++ Draw/Dice/Controller/Room/DiceRoomListController.m
@@ -202,6 +202,7 @@
     [self.friendRoomButton.fontLable setTextColor:[UIColor whiteColor]]; 
     [self.nearByRoomButton.fontLable setTextColor:[UIColor whiteColor]]; 
     
+    [self.titleFontButton.fontLable setText:NSLS(@"kDiceRoomListTitle")];
     [self.allRoomButton.fontLable setText:NSLS(@"kAll")];
     [self.friendRoomButton.fontLable setText:NSLS(@"kFriend")];
     [self.nearByRoomButton.fontLable setText:NSLS(@"kNearBy")];
