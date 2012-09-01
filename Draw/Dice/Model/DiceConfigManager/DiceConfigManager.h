7e7ca3d80e5af11eda21e3dabfcb790a5d9f12ca
xiaotaowang <445242970@qq.com>
2012-09-01 12:22:34 +0800

    commit

---
+++ Draw/Dice/Model/DiceConfigManager/DiceConfigManager.h
@@ -0,0 +1,13 @@
+//
+//  DiceConfigManager.h
+//  Draw
+//
+//  Created by 小涛 王 on 12-9-1.
+//  Copyright (c) 2012年 甘橙软件. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DiceConfigManager : NSObject
+
+@end
---
+++ Draw/Dice/Model/DiceConfigManager/DiceConfigManager.m
@@ -0,0 +1,13 @@
+//
+//  DiceConfigManager.m
+//  Draw
+//
+//  Created by 小涛 王 on 12-9-1.
+//  Copyright (c) 2012年 甘橙软件. All rights reserved.
+//
+
+#import "DiceConfigManager.h"
+
+@implementation DiceConfigManager
+
+@end
