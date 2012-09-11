e34ffbd9009e065c6adffd34e2c755b0d7285edc
xiaotaowang <445242970@qq.com>
2012-09-11 16:56:58 +0800

    commit

---
+++ Draw/Dice/Controller/Play/RollAgainItemAction.h
@@ -0,0 +1,13 @@
+//
+//  RollAgainItemAction.h
+//  Draw
+//
+//  Created by 小涛 王 on 12-9-11.
+//  Copyright (c) 2012年 甘橙软件. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RollAgainItemAction : NSObject
+
+@end
