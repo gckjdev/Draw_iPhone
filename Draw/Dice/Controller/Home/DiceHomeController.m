bb2e019c9e1a87831307b1dc7eb5a6dca871d2c5
GamyHuang <hguangm2009@gmail.com>
2012-09-15 12:01:27 +0800

    update menu panel.

--- Draw/Dice/Controller/Home/DiceHomeController.m
+++ Draw/Dice/Controller/Home/DiceHomeController.m
@@ -99,7 +99,7 @@
     self.menuPanel = [MenuPanel menuPanelWithController:self 
                                             gameAppType:GameAppTypeDice];
     
-    self.menuPanel.center = [DeviceDetection isIPAD] ? CGPointMake(384, 682) : CGPointMake(160, 304);
+    self.menuPanel.center = [DeviceDetection isIPAD] ? CGPointMake(384, 686) : CGPointMake(160, 306);
     
     [self.view insertSubview:self.menuPanel atIndex:0];
     
@@ -110,7 +110,7 @@
     self.bottomMenuPanel = [BottomMenuPanel panelWithController:self
                                                     gameAppType:GameAppTypeDice];
     
-    self.bottomMenuPanel.center = [DeviceDetection isIPAD] ? CGPointMake(384, 960) : CGPointMake(160, 439);
+    self.bottomMenuPanel.center = [DeviceDetection isIPAD] ? CGPointMake(384, 961) : CGPointMake(160, 438);
     
     [self.view addSubview:_bottomMenuPanel];
 }
