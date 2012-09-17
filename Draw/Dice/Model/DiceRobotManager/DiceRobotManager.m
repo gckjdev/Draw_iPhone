889bf8710daeefd3a000e7b6eb9733019aaaca91
kira <ouyongyong@163.com>
2012-09-17 10:55:49 +0800

    add dice robot

---
+++ Draw/Dice/Model/DiceRobotManager/DiceRobotManager.m
@@ -0,0 +1,13 @@
+//
+//  DiceRobotManager.m
+//  Draw
+//
+//  Created by Orange on 12-9-17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "DiceRobotManager.h"
+
+@implementation DiceRobotManager
+
+@end
