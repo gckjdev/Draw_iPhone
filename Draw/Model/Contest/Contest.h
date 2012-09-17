f76be4635ac155376ff10540a0585d584f17d5b1
GamyHuang <hguangm2009@gmail.com>
2012-09-17 16:39:53 +0800

    add contest

---
+++ Draw/Model/Contest/Contest.h
@@ -0,0 +1,65 @@
+//
+//  Contest.h
+//  Draw
+//
+//  Created by  on 12-9-17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+typedef enum{
+    
+    ContestStatusPending = 1,
+    ContestStatusRunning = 2,
+    ContestStatusPassed = 3,    
+}ContestStatus;
+
+typedef enum{    
+    ContestTypeFree = 1,
+    ContestTypesTopic = 2,
+    ContestTypeusWord = 3,    
+    ContestTypeusWordList = 4,        
+}ContestType;
+
+@interface Contest : NSObject
+{
+    NSString *_contestId;
+    NSDate *_startDate;
+    NSDate *_endDate;
+    NSString *_version;
+    ContestType _type;
+    ContestStatus _status;
+    NSInteger _participantCount;
+    NSInteger _opusCount;
+    
+    NSString *_topicDesc;
+    NSString *_awardDesc;
+    NSString *_ruleDesc;
+
+    NSString *_title;
+    NSString *_imageUrl;
+
+}
+
+//basic info
+@property(nonatomic, retain) NSString *contestId;
+@property(nonatomic, retain) NSDate *startDate;
+@property(nonatomic, retain) NSDate *endDate;
+@property(nonatomic, retain) NSString *version;
+@property(nonatomic, assign) ContestType type;
+@property(nonatomic, assign) ContestStatus status;
+@property(nonatomic, assign) NSInteger participantCount;
+@property(nonatomic, assign) NSInteger opusCount;
+@property(nonatomic, retain) NSString *title;
+@property(nonatomic, retain) NSString *imageUrl;
+
+//desc info
+@property(nonatomic, retain) NSString *topicDesc;
+@property(nonatomic, retain) NSString *awardDesc;
+@property(nonatomic, retain) NSString *ruleDesc;
+
+
+
+@end
