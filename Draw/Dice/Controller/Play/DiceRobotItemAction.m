36084b8e05d08c98d6e5cae2beea82a773d93d24
kira <ouyongyong@163.com>
2012-09-18 16:57:58 +0800

    add item

---
+++ Draw/Dice/Controller/Play/DiceRobotItemAction.m
@@ -0,0 +1,13 @@
+//
+//  DiceRobotItemAction.m
+//  Draw
+//
+//  Created by Orange on 12-9-18.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "DiceRobotItemAction.h"
+
+@implementation DiceRobotItemAction
+
+@end
