fff95536d90c9cf8debd0e965b0a1d0a2b9f3689
kira <ouyongyong@163.com>
2012-09-19 14:38:40 +0800

    add common search view

---
+++ Draw/Dice/View/CommonView/CommonSearchView.h
@@ -0,0 +1,13 @@
+//
+//  CommonSearchView.h
+//  Draw
+//
+//  Created by Orange on 12-9-19.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "CommonInfoView.h"
+
+@interface CommonSearchView : CommonInfoView
+
+@end
