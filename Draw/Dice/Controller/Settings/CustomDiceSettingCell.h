f8eabd632dba729d71ec89dc582583ea7fb90804
kira <ouyongyong@163.com>
2012-09-20 17:13:11 +0800

    add custom dice setting controller

---
+++ Draw/Dice/Controller/Settings/CustomDiceSettingCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomDiceSettingCell.h
+//  Draw
+//
+//  Created by Orange on 12-9-20.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "PPTableViewCell.h"
+
+@interface CustomDiceSettingCell : PPTableViewCell
+
+@end
