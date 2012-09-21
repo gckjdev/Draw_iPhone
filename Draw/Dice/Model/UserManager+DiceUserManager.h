d434ad28fe78ad59986affe0d338b04e2c02595d
kira <ouyongyong@163.com>
2012-09-21 15:25:27 +0800

    add usermanager category

---
+++ Draw/Dice/Model/UserManager+DiceUserManager.h
@@ -0,0 +1,13 @@
+//
+//  UserManager+DiceUserManager.h
+//  Draw
+//
+//  Created by Orange on 12-9-21.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "UserManager.h"
+
+@interface UserManager (DiceUserManager)
+
+@end
---
+++ Draw/Dice/Model/UserManager+DiceUserManager.m
@@ -0,0 +1,13 @@
+//
+//  UserManager+DiceUserManager.m
+//  Draw
+//
+//  Created by Orange on 12-9-21.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "UserManager+DiceUserManager.h"
+
+@implementation UserManager (DiceUserManager)
+
+@end
