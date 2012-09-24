f755d9499f7f36b4668de146ce01ff827fb272c1
kira <ouyongyong@163.com>
2012-09-24 15:17:39 +0800

    add dice robot decision view

---
+++ Draw/Dice/View/DiceRobotDecisionView/DiceRobotDecisionView.h
@@ -0,0 +1,13 @@
+//
+//  DiceRobotDecisionView.h
+//  Draw
+//
+//  Created by Orange on 12-9-24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "CommonDialog.h"
+
+@interface DiceRobotDecisionView : CommonDialog
+
+@end
---
+++ Draw/Dice/View/DiceRobotDecisionView/DiceRobotDecisionView.m
@@ -0,0 +1,31 @@
+//
+//  DiceRobotDecisionView.m
+//  Draw
+//
+//  Created by Orange on 12-9-24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "DiceRobotDecisionView.h"
+
+@implementation DiceRobotDecisionView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
