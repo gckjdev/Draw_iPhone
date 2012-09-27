1423c994aef39a2749646537928326ac5d1d8212
Benson Peng <penglingzhe@gmail.com>
2012-09-27 23:58:58 +0800

    Dice 2.0 submission

--- Draw/Model/Config/ConfigManager.m
+++ Draw/Model/Config/ConfigManager.m
@@ -336,20 +336,20 @@
 + (NSString*)getDiceServerListStringWithNormal
 {
 //    return @"192.168.1.10:8080";
-    return @"58.215.172.169:8080";
+//    return @"58.215.172.169:8080";
     return [MobClickUtils getStringValueByKey:@"DICE_SERVER_LIST" defaultValue:@"58.215.164.153:8018"];
 }
 
 + (NSString *)getDiceServerListStringWithHightRule
 {
-    return @"58.215.172.169:8081";
-    return [MobClickUtils getStringValueByKey:@"DICE_SERVER_LIST_HIGHT" defaultValue:@"58.215.164.153:8081"];
+//    return @"58.215.172.169:8081";
+    return [MobClickUtils getStringValueByKey:@"DICE_SERVER_LIST_HIGHT" defaultValue:@"58.215.164.153:8019"];
 }
 
 + (NSString *)getDiceServerListStringWithSuperHightRule
 {
-    return @"58.215.172.169:8082";
-    return [MobClickUtils getStringValueByKey:@"DICE_SERVER_LIST_SUPER_HIGHT" defaultValue:@"58.215.164.153:8082"];
+//    return @"58.215.172.169:8082";
+    return [MobClickUtils getStringValueByKey:@"DICE_SERVER_LIST_SUPER_HIGHT" defaultValue:@"58.215.164.153:8020"];
 }
 
 + (int)getBetAnteWithNormalRule
