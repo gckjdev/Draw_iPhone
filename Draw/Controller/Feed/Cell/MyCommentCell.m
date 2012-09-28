098acccd73b13ae5f70f74c8597357806988837a
GamyHuang <hguangm2009@gmail.com>
2012-09-28 17:09:05 +0800

    add my comment.

---
+++ Draw/Controller/Feed/Cell/MyCommentCell.m
@@ -0,0 +1,168 @@
+//
+//  CommentCell.m
+//  Draw
+//
+//  Created by  on 12-6-13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "MyCommentCell.h"
+#import "TimeUtils.h"
+#import "LocaleUtils.h"
+#import "DeviceDetection.h"
+#import "LocaleUtils.h"
+#import "WordManager.h"
+#import "CommonUserInfoView.h"
+#import "ShareImageManager.h"
+#import "CommentFeed.h"
+
+@implementation MyCommentCell
+@synthesize commentLabel;
+@synthesize timeLabel;
+@synthesize nickNameLabel;
+@synthesize itemImage;
+@synthesize splitLine;
+@synthesize feed = _feed;
+
++ (id)createCell:(id)delegate
+{
+    NSString* cellId = [self getCellIdentifier];
+    NSArray *topLevelObjects = [[NSBundle mainBundle] loadNibNamed:cellId owner:self options:nil];
+    if (topLevelObjects == nil || [topLevelObjects count] <= 0){
+        return nil;
+    }
+    
+    ((PPTableViewCell*)[topLevelObjects objectAtIndex:0]).delegate = delegate;
+    
+    return [topLevelObjects objectAtIndex:0];
+}
+
++ (NSString*)getCellIdentifier
+{
+    return @"CommentCell";
+}
+
+
+#define COMMENT_WIDTH ([DeviceDetection isIPAD] ? 500 : 204)
+#define COMMENT_FONT_SIZE ([DeviceDetection isIPAD] ? 11*2 : 11)
+#define COMMENT_SPACE ([DeviceDetection isIPAD] ? 20 : 10)
+#define COMMENT_BASE_X ([DeviceDetection isIPAD] ? 102 : 44)
+#define COMMENT_BASE_Y ([DeviceDetection isIPAD] ? 65 : 30)
+
+#define COMMENT_ITEM_HEIGHT ([DeviceDetection isIPAD] ? 110 : 60)
+
+#define AVATAR_VIEW_FRAME [DeviceDetection isIPAD] ? CGRectMake(12, 10, 74, 77) : CGRectMake(5, 9, 31, 32)
+
+
+- (IBAction)clickReplyButton:(id)sender {
+    if (self.delegate && [self.delegate 
+                          respondsToSelector:@selector(didStartToReplyToFeed:)]) {
+        [self.delegate didStartToReplyToFeed:self.feed];
+    }
+}
+
++ (CGFloat)getCellHeight:(CommentFeed *)feed
+{
+    if (feed.feedType == ItemTypeFlower || feed.feedType == ItemTypeTomato) {
+        return COMMENT_ITEM_HEIGHT;
+    }
+    NSString *comment = [feed commentInFeedDeatil];
+    UIFont *font = [UIFont systemFontOfSize:COMMENT_FONT_SIZE];
+    CGSize commentSize = [comment sizeWithFont:font constrainedToSize:CGSizeMake(COMMENT_WIDTH, 10000000) lineBreakMode:UILineBreakModeCharacterWrap];
+    CGFloat height = COMMENT_BASE_Y + COMMENT_SPACE + commentSize.height;
+//    PPDebug(@"comment = %@,height = %f", comment,height);
+    return height;
+}
+
+#define SHOW_COMMENT_COUNT 3
+
+- (void)setCellInfo:(CommentFeed *)feed;
+{
+    //set avatar
+    self.feed = feed;
+    
+    FeedUser *author = feed.author;
+    [_avatarView removeFromSuperview];
+    _avatarView = [[AvatarView alloc] initWithUrlString:author.avatar 
+                                                  frame:AVATAR_VIEW_FRAME 
+                                                 gender:author.gender 
+                                                  level:0];
+    _avatarView.delegate = self;
+    _avatarView.userId = feed.author.userId;
+    [self addSubview:_avatarView];
+    [_avatarView release];
+    
+    //set times
+    NSString *timeString = nil;
+    if ([LocaleUtils isChinese]) {
+        timeString = chineseBeforeTime(feed.createDate);
+    } else {
+        timeString = englishBeforeTime(feed.createDate);
+    }
+    
+    if (timeString) {
+        [self.timeLabel setText:timeString];
+    }else {
+        NSString *formate = @"yy-MM-dd HH:mm";
+        timeString = dateToStringByFormat(feed.createDate, formate);
+        [self.timeLabel setText:timeString];
+    }
+    
+     NSString *comment = [feed commentInFeedDeatil];
+    //set user name
+    [self.nickNameLabel setText:author.nickName];
+    
+    itemImage.hidden = YES;
+    commentLabel.hidden = NO;
+    if (feed.feedType == ItemTypeFlower) {
+        itemImage.hidden = NO;
+        [itemImage setImage:[[ShareImageManager defaultManager] flower]];
+        
+    }else if(feed.feedType == ItemTypeTomato)
+    {
+        itemImage.hidden = NO;
+        [itemImage setImage:[[ShareImageManager defaultManager] tomato]];
+    }
+        
+    //set comment
+
+    UIFont *font = [UIFont systemFontOfSize:COMMENT_FONT_SIZE];
+    CGSize commentSize = [comment sizeWithFont:font constrainedToSize:CGSizeMake(COMMENT_WIDTH, 10000000) lineBreakMode:UILineBreakModeCharacterWrap];
+    
+    self.commentLabel.frame = CGRectMake(COMMENT_BASE_X, COMMENT_BASE_Y, COMMENT_WIDTH,commentSize.height);
+        
+    [self.commentLabel setText:[NSString stringWithFormat:@"%@", comment]];
+    [self.commentLabel setFont:font];
+    
+    CGFloat y = COMMENT_BASE_Y + COMMENT_SPACE + commentSize.height - 0.5;
+    CGFloat x = splitLine.center.x;
+    splitLine.center = CGPointMake(x, y);
+//    PPDebug(@"center = (%f,%f)",x,y);
+}
+
+
+
+- (void)dealloc {
+    PPRelease(commentLabel);
+    PPRelease(timeLabel);
+    PPRelease(nickNameLabel);
+    PPRelease(itemImage);
+    PPRelease(splitLine);
+    [super dealloc];
+}
+
+#pragma mark - avatar view delegate
+- (void)didClickOnAvatar:(NSString *)userId
+{
+    [CommonUserInfoView showUser:userId 
+                        nickName:nil 
+                          avatar:nil 
+                          gender:nil 
+                        location:nil 
+                           level:1
+                         hasSina:NO 
+                           hasQQ:NO 
+                     hasFacebook:NO 
+                      infoInView:self.delegate];
+}
+@end
