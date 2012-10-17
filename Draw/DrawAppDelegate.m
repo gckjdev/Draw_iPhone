ecb09c048cc8e23d0a32ceb3bdcf24cc70344f83
GamyHuang <hguangm2009@gmail.com>
2012-10-17 18:05:38 +0800

    add new image..

--- Draw/DrawAppDelegate.m
+++ Draw/DrawAppDelegate.m
@@ -69,8 +69,8 @@ NSString* GlobalGetTrafficServerURL()
 
 NSString* GlobalGetBoardServerURL()
 {
-//    return [ConfigManager getTrafficAPIServerURL];
-    return @"http://192.168.1.13:8100/api/i?";    
+    return [ConfigManager getTrafficAPIServerURL];
+//    return @"http://192.168.1.13:8100/api/i?";    
 }
 
 
