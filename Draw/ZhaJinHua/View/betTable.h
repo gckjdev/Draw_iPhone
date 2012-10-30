6e637282b97c354147cb665870596710614d4103
kira <ouyongyong@163.com>
2012-10-30 15:12:42 +0800

    update

---
+++ Draw/ZhaJinHua/View/betTable.h
@@ -0,0 +1,13 @@
+//
+//  betTable.h
+//  Draw
+//
+//  Created by Kira on 12-10-30.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface betTable : UIView
+
+@end
---
+++ Draw/ZhaJinHua/View/betTable.m
@@ -0,0 +1,31 @@
+//
+//  betTable.m
+//  Draw
+//
+//  Created by Kira on 12-10-30.
+//
+//
+
+#import "betTable.h"
+
+@implementation betTable
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
