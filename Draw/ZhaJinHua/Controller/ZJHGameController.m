1f4ecffc1ac6610762a5248cc9aea144402f8404
kira <ouyongyong@163.com>
2012-10-30 14:08:25 +0800

    update

--- Draw/ZhaJinHua/Controller/ZJHGameController.m
+++ Draw/ZhaJinHua/Controller/ZJHGameController.m
@@ -27,6 +27,8 @@
 @interface ZJHGameController ()
 
 - (void)roomChanged;
+- (ZJHPokerView*)getSelfPokersView;
+- (ZJHPokerView*)getPokersViewByPosition:(UserPosition)position;
 
 @end
 
@@ -248,12 +250,21 @@
 
 - (void)someOneCheckCard:(UserPosition)position
 {
-    
+    ZJHPokerView* view = [self getPokersViewByPosition:position];
+    ZJHPokerSectorType type;
+    if (position == UserPositionLeft || position == UserPositionLeftTop) {
+        type = ZJHPokerSectorTypeLeft;
+    }
+    if (position == UserPositionRight || position == UserPositionRightTop) {
+        type = ZJHPokerSectorTypeRight;
+    }
+    [view makeSectorShape:type animation:YES];
 }
 
 - (void)someOneFoldCard:(UserPosition)position
 {
-    
+    ZJHPokerView* view = [self getPokersViewByPosition:position];
+    [view fold:YES];
 }
 
 - (ZJHPokerView*)getSelfPokersView
@@ -264,6 +275,18 @@
 - (ZJHPokerView*)getPokersViewByPosition:(UserPosition)position
 {
     return (ZJHPokerView*)[self.view viewWithTag:(POKERS_TAG_OFFSET+position)];
+}
+
+- (UserPosition)getPositionByUserId:(NSString*)userId
+{
+    PBGameUser* user = [_gameService.session getUserByUserId:userId];
+    PBGameUser* selfUser = [self getselfUser];
+    return (UserPositionCenter + (user.seatId - selfUser.seatId))%MAX_PLAYER_COUNT;
+}
+
+- (ZJHPokerView*)getPokersViewByUserId:(NSString*)userId
+{
+    return [self getPokersViewByPosition:[self getPositionByUserId:userId]];
 }
 
 @end
