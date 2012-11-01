50dd7ac31ecf20f206214e25c9d04396970956aa
xiaotaowang <445242970@qq.com>
2012-11-01 16:19:57 +0800

    commit

---
+++ Draw/ZhaJinHua/View/ChipsSelectView.h
@@ -0,0 +1,13 @@
+//
+//  ChipsSelectView.h
+//  Draw
+//
+//  Created by 王 小涛 on 12-11-1.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChipsSelectView : UIView
+
+@end
---
+++ Draw/ZhaJinHua/View/ChipsSelectView.m
@@ -0,0 +1,31 @@
+//
+//  ChipsSelectView.m
+//  Draw
+//
+//  Created by 王 小涛 on 12-11-1.
+//
+//
+
+#import "ChipsSelectView.h"
+
+@implementation ChipsSelectView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
