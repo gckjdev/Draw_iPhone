2aa48b6b8229cb1975f5cfc260aa7143c2b61d4d
kira <ouyongyong@163.com>
2012-11-01 17:02:39 +0800

    add dealer view

---
+++ Draw/ZhaJinHua/View/DealerView.m
@@ -0,0 +1,31 @@
+//
+//  DealerView.m
+//  Draw
+//
+//  Created by Kira on 12-11-1.
+//
+//
+
+#import "DealerView.h"
+
+@implementation DealerView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
