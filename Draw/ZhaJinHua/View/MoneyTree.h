6487242c50755e424e1e33c52f16d66735bb3f21
kira <ouyongyong@163.com>
2012-11-05 14:47:03 +0800

    add money tree

---
+++ Draw/ZhaJinHua/View/MoneyTree.h
@@ -0,0 +1,13 @@
+//
+//  MoneyTree.h
+//  Draw
+//
+//  Created by Kira on 12-11-5.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoneyTree : UIButton
+
+@end
---
+++ Draw/ZhaJinHua/View/MoneyTree.m
@@ -0,0 +1,31 @@
+//
+//  MoneyTree.m
+//  Draw
+//
+//  Created by Kira on 12-11-5.
+//
+//
+
+#import "MoneyTree.h"
+
+@implementation MoneyTree
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
