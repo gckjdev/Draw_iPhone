68f615dc708b50fd70ebb72f053fd20ba4da3c3c
kira <ouyongyong@163.com>
2012-11-05 18:14:13 +0800

    update

--- Draw/ZhaJinHua/View/BetTable.m
+++ Draw/ZhaJinHua/View/BetTable.m
@@ -122,15 +122,22 @@
     //播放当前筹码动画，把筹码压进清除队列
     while ([_visibleLayerQueue peek] != nil) {
         CALayer* layer = [_visibleLayerQueue dequeue];
-        CAAnimation* anim = [AnimationManager translationAnimationTo:[self getPointByPosition:position] duration:1];
-        CAAnimation* anim2 = [AnimationManager missingAnimationWithDuration:1];
-        anim.removedOnCompletion = NO;
-        anim2.removedOnCompletion = NO;
-        [layer addAnimation:anim forKey:nil];
-        [CATransaction setCompletionBlock:^{
-            [layer setPosition:[self getPointByPosition:position]];
-            [layer setOpacity:0];
+        [UIView animateWithDuration:2 animations:^{
+            layer.position = [self getPointByPosition:position];
+        } completion:^(BOOL finished) {
+            [UIView animateWithDuration:1 animations:^{
+                layer.opacity = 0;
+            }];
         }];
+//        CAAnimation* anim = [AnimationManager translationAnimationTo:[self getPointByPosition:position] duration:1];
+//        CAAnimation* anim2 = [AnimationManager missingAnimationWithDuration:1];
+//        anim.removedOnCompletion = NO;
+//        anim2.removedOnCompletion = NO;
+//        [layer addAnimation:anim forKey:nil];
+//        [CATransaction setCompletionBlock:^{
+//            [layer setPosition:[self getPointByPosition:position]];
+//            [layer setOpacity:0];
+//        }];
         [_layerQueue enqueue:layer];
         PPDebug(@"<test>enqueue, count = %d",_layerQueue.count);
     }
