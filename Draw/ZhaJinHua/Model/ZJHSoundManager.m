7c633a90eb2676674184945202c0b07d78fed004
kira <ouyongyong@163.com>
2012-11-07 15:34:05 +0800

    update

--- Draw/ZhaJinHua/Model/ZJHSoundManager.m
+++ Draw/ZhaJinHua/Model/ZJHSoundManager.m
@@ -22,22 +22,30 @@ static ZJHSoundManager* shareInstance;
 
 - (NSString*)betSoundEffect
 {
-    
+    return @"bet.mp3";
 }
 
 - (NSString*)betHumanSound:(BOOL)gender
 {
-    
+    if (gender) {
+        return [NSString stringWithFormat:@"bet%d_M.mp3",rand()%3+1];
+    } else {
+        return [NSString stringWithFormat:@"bet%d_F.mp3",rand()%4+1];
+    }
 }
 
 - (NSString*)checkCardSoundEffect
 {
-    
+    return @"check_card.mp3";
 }
 
 - (NSString*)checkCardHumanSound:(BOOL)gender
 {
-    
+    if (gender) {
+        return @"check_card1.mp3";
+    } else {
+        return [NSString stringWithFormat:@"check_card%d_F.mp3",rand()%2+1];
+    }
 }
 
 - (NSString*)compareCardSoundEffect
@@ -47,7 +55,11 @@ static ZJHSoundManager* shareInstance;
 
 - (NSString*)compareCardHumanSound:(BOOL)gender
 {
-    
+    if (gender) {
+        return @"compare_card1_M.mp3";
+    } else {
+        return @"compare_card1_F.mp3";
+    }
 }
 
 - (NSString*)foldCardSoundEffect
@@ -57,72 +69,80 @@ static ZJHSoundManager* shareInstance;
 
 - (NSString*)foldCardHumanSound:(BOOL)gender
 {
-    
+    if (gender) {
+        return @"fold_card1_M.mp3";
+    } else {
+        return [NSString stringWithFormat:@"fold_card%d_F.mp3",rand()%2+1];
+    }
 }
 
 - (NSString*)raiseBetSoundEffect
 {
-    
+    return @"raise_bet.mp3";
 }
 
 - (NSString*)raiseBetHumanSound:(BOOL)gender
 {
-    
+    if (gender) {
+        return @"raise_bet1_M.mp3";
+    } else {
+        return @"raise_bet1_F.mp3";
+    }
 }
 
 - (NSString*)clickButtonSound
 {
-    
+    return @"click_button.mp3";
 }
 
 - (NSString*)dealCardAppear
 {
-    
+    return @"dealer_appear.mp3";
 }
 
 - (NSString*)dealCard
 {
-    
+    return @"deal_card.mp3";
 }
 
 - (NSString*)dealCardDisappear
 {
-    
+    return @"dealer_disappear.mp3";
 }
 
 - (NSString*)fireworks
 {
-    
+    return @"fireworks.mp3";
 }
 
 - (NSString*)flee
 {
-    
+    return @"flee.mp3";
 }
 
 - (NSString*)fullMoney
 {
-    
+    return @"full_monney.mp3";
 }
 
 - (NSString*)gameBGM
 {
-    
+    return @"game_bg.mp3";
 }
 
 - (NSString*)gameOver
 {
-    
+    return @"game_over.mp3";
 }
 
 - (NSString*)gameWin
 {
-    
+    return @"game_win.mp3";
 }
 
 - (NSString*)getChips
 {
-    
+    return @"get_chips.mp3";
 }
 
 @end
