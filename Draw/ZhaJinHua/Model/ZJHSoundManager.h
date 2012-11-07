cbc601c788f472f3aa3b8d161fe56a0281edfc52
kira <ouyongyong@163.com>
2012-11-07 12:08:58 +0800

    add zjh sound manager

---
+++ Draw/ZhaJinHua/Model/ZJHSoundManager.h
@@ -0,0 +1,13 @@
+//
+//  ZJHSoundManager.h
+//  Draw
+//
+//  Created by Kira on 12-11-7.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZJHSoundManager : NSObject
+
+@end
---
+++ Draw/ZhaJinHua/Model/ZJHSoundManager.m
@@ -0,0 +1,13 @@
+//
+//  ZJHSoundManager.m
+//  Draw
+//
+//  Created by Kira on 12-11-7.
+//
+//
+
+#import "ZJHSoundManager.h"
+
+@implementation ZJHSoundManager
+
+@end
