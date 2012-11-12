79cbd5ea58bc87273847322dfd05b33747615736
xiaotaowang <445242970@qq.com>
2012-11-12 16:02:06 +0800

    commit

---
+++ Draw/ZhaJinHua/View/MessageView.h
@@ -0,0 +1,18 @@
+//
+//  MessageView.h
+//  Draw
+//
+//  Created by 王 小涛 on 12-11-12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageView : UIView
+
++ messageViewWithMessage:(NSString *)message
+                    font:(UIFont *)font
+           textAlignment:(UITextAlignment)textAlignment
+                 bgImage:(UIImage*)bgImage;
+
+@end
