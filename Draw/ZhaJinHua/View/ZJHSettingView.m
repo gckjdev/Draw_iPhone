2ba3b9d7ffab7bec993dadcd2733269d37b189ff
kira <ouyongyong@163.com>
2012-11-13 12:23:11 +0800

    add setting

---
+++ Draw/ZhaJinHua/View/ZJHSettingView.m
@@ -0,0 +1,31 @@
+//
+//  ZJHSettingView.m
+//  Draw
+//
+//  Created by Kira on 12-11-13.
+//
+//
+
+#import "ZJHSettingView.h"
+
+@implementation ZJHSettingView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
