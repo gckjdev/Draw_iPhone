4a3840ea7bcd654acc7ba158ee2953081555c1cf
xiaotaowang <445242970@qq.com>
2012-11-14 13:37:06 +0800

    commit

---
+++ Draw/ZhaJinHua/Controller/ZJHScreenConfig.h
@@ -0,0 +1,13 @@
+//
+//  ZJHScreenConfig.h
+//  Draw
+//
+//  Created by 王 小涛 on 12-11-14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZJHScreenConfig : NSObject
+
+@end
---
+++ Draw/ZhaJinHua/Controller/ZJHScreenConfig.m
@@ -0,0 +1,13 @@
+//
+//  ZJHScreenConfig.m
+//  Draw
+//
+//  Created by 王 小涛 on 12-11-14.
+//
+//
+
+#import "ZJHScreenConfig.h"
+
+@implementation ZJHScreenConfig
+
+@end
