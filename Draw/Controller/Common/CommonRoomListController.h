920135162219c629f5c01e983ae588fc6ebc89b0
kira <ouyongyong@163.com>
2012-11-14 10:24:50 +0800

    add common room list

--- Draw/Controller/Common/CommonRoomListController.h
+++ Draw/Controller/Common/CommonRoomListController.h
@@ -7,7 +7,33 @@
 //
 
 #import "PPTableViewController.h"
+#import "PPTableViewController.h"
+#import "RoomPasswordDialog.h"
+#import "CommonDialog.h"
+#import "CommonSearchView.h"
+#import "CommonGameNetworkService.h"
 
-@interface CommonRoomListController : PPTableViewController
+typedef enum {
+    allRoom = 0,
+    friendRoom = 1,
+    nearByRoom = 2
+}RoomFilter;
+
+@class PBGameSession;
+@class CommonSearchView;
+
+@interface CommonRoomListController : PPTableViewController</*CommonGameServiceDelegate, */InputDialogDelegate, CommonDialogDelegate, CommonSearchViewDelegate, CommonInfoViewDelegate> {
+    BOOL _isJoiningDice;
+    PBGameSession* _currentSession;
+    NSTimer* _refreshRoomTimer;
+    BOOL firstLoad;
+    int _currentRoomType;
+    CommonSearchView* _searchView;
+    BOOL _isRefreshing;
+    CommonGameNetworkService *_gameService;
+}
+
+
+@property (retain, nonatomic) PBGameSession* currentSession;
 
 @end
--- Draw/Controller/Common/CommonRoomListController.m
+++ Draw/Controller/Common/CommonRoomListController.m
@@ -7,12 +7,33 @@
 //
 
 #import "CommonRoomListController.h"
+#import "CommonGameNetworkClient.h"
+#import "NotificationName.h"
+#import "GameMessage.pb.h"
+#import "GameConstants.pb.h"
+#import "CommonMessageCenter.h"
+#import "AccountService.h"
+#import "ConfigManager.h"
+#import "CoinShopController.h"
+#import "LmWallService.h"
+#import "CommonRoomListCell.h"
+#import "UserManager.h"
+
+
+#define KEY_GAME_MESSAGE @"KEY_GAME_MESSAGE"
+#define ROOMS_COUNT_PER_PAGE  20
+
+#define REFRESH_ROOMS_TIME_INTERVAL 2
+
+#define CREATE_ROOM_DIALOG_TAG  120120824
+#define ENTER_ROOM_DIALOG_TAG   220120824
 
 @interface CommonRoomListController ()
 
 @end
 
 @implementation CommonRoomListController
+@synthesize currentSession;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -23,10 +44,12 @@
     return self;
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
+- (id)init {
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,5 +57,420 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)dealloc {
+    _searchView.disappearDelegate = nil;
+    [self clearRefreshRoomsTimer];
+    PPRelease(_currentSession);
+    [super dealloc];
+}
+
+- (BOOL)meetJoinGameCondition
+{
+    PPDebug(@"<CommonRoomListController> meetJoinGameCondition method not implement");
+    return NO;
+}
+
+- (void)checkAndJoinGame:(int)sessionId
+{
+    PPDebug(@"<CommonRoomListController> checkAndJoinGame:(int)sessionId method not implement");
+}
+
+- (void)checkAndJoinGame
+{
+    PPDebug(@"<CommonRoomListController> checkAndJoinGame method not implement");
+}
+
+- (void)createRoomWithName:(NSString*)targetText
+                  password:(NSString*)password
+{
+    [self showActivityWithText:NSLS(@"kCreatingRoom")];
+}
+
+- (void)refreshRooms:(id)sender
+{
+    [_gameService getRoomList:0 count:ROOMS_COUNT_PER_PAGE];
+}
+
+- (void)clearRefreshRoomsTimer
+{
+    if (_refreshRoomTimer) {
+        if ([_refreshRoomTimer isValid]) {
+            [_refreshRoomTimer invalidate];
+        }
+        [_refreshRoomTimer release];
+        _refreshRoomTimer = nil;
+    }
+}
+
+- (void)startRefreshRoomsTimer
+{
+    [self clearRefreshRoomsTimer];
+    _refreshRoomTimer = [NSTimer scheduledTimerWithTimeInterval:REFRESH_ROOMS_TIME_INTERVAL
+                                                         target:self
+                                                       selector:@selector(refreshRooms:)
+                                                       userInfo:nil
+                                                        repeats:NO];
+    [_refreshRoomTimer retain];
+}
+
+- (void)pauseRefreshingRooms
+{
+    _isRefreshing = NO;
+    [self clearRefreshRoomsTimer];
+}
+
+- (void)continueRefreshingRooms
+{
+    _isRefreshing = YES;
+    [self startRefreshRoomsTimer];
+}
+
+- (void)updateRoomList
+{
+//    self.emptyListTips.hidden = YES;
+//    if (self.dataList.count < 1) {
+//        self.emptyListTips.hidden = NO;
+//        if (_currentRoomType == friendRoom) {
+//            [self.emptyListTips setText:NSLS(@"kNoFriendRoom")];
+//        }
+//        if (_searchView) {
+//            [self.emptyListTips setText:NSLS(@"kSearchEmpty")];
+//        }
+//    }
+    PPDebug(@"<CommonRoomListController> updateRoomList method not implement");
+}
+
+
+- (void)getRoomsFinished
+{
+    [self hideActivity];
+    self.dataList = [NSArray arrayWithArray:_gameService.roomList];
+    [self.dataTableView reloadData];
+    //[[DiceGameService defaultService] registerRoomsNotification:service.roomList]; //don register room notification here
+    //self.noMoreData = YES;
+    //[self dataSourceDidFinishLoadingMoreData];
+    if (_isRefreshing) {
+        [self startRefreshRoomsTimer];
+    }
+    [self updateRoomList];
+    [self updateOnlineUserCount];
+}
+
+- (void)joinGame
+{
+    [self hideActivity];
+    PPDebug(@"<CommonRoomListController> joinGame method not implement");
+}
+
+- (void)connectServer
+{
+    PPDebug(@"<CommonRoomListController> connectServer method not implement");
+}
+
+- (void)registerDiceRoomNotification
+{
+    
+    [self registerNotificationWithName:NOTIFICAIION_CREATE_ROOM_RESPONSE
+                            usingBlock:^(NSNotification *note) {
+                                PPDebug(@"<DiceRoomListController> NOTIFICAIION_CREATE_ROOM_RESPONSE");
+                                [self hideActivity];
+                                GameMessage* message = [CommonGameNetworkService userInfoToMessage:note.userInfo];
+                                if (message.resultCode == GameResultCodeSuccess) {
+                                    [self joinGame];
+                                } else if (message.resultCode == GameResultCodeErrorSessionNameDuplicated) {
+                                    [[CommonMessageCenter defaultCenter] postMessageWithText:NSLS(@"kRoomNameDuplicated") delayTime:2 isHappy:NO];
+                                }else {
+                                    [[CommonMessageCenter defaultCenter] postMessageWithText:NSLS(@"kJoinGameFailure") delayTime:1.5 isHappy:NO];
+                                }
+                            }];
+    
+    [self registerNotificationWithName:NOTIFICAIION_GET_ROOMS_RESPONSE
+                            usingBlock:^(NSNotification *note) {
+                                PPDebug(@"<DiceRoomListController> NOTIFICAIION_GET_ROOMS_RESPONSE");
+                                GameMessage* message = [CommonGameNetworkService userInfoToMessage:note.userInfo];
+                                if (message.resultCode == GameResultCodeSuccess) {
+                                    [self getRoomsFinished];
+                                }
+                            }];
+    [self registerNotificationWithName:NOTIFICATION_JOIN_GAME_RESPONSE
+                            usingBlock:^(NSNotification *note) {
+                                PPDebug(@"<DiceRoomListController> NOTIFICATION_JOIN_GAME_RESPONSE");
+                                [self hideActivity];
+                                GameMessage* message = [CommonGameNetworkService userInfoToMessage:note.userInfo];
+                                if (message.resultCode == GameResultCodeSuccess) {
+                                    [self joinGame];
+                                } else if (message.resultCode == GameResultCodeErrorSessionidFull) {
+                                    [[CommonMessageCenter defaultCenter] postMessageWithText:NSLS(@"kSessionFull") delayTime:1.5 isHappy:NO];
+                                } else {
+                                    [[CommonMessageCenter defaultCenter] postMessageWithText:NSLS(@"kJoinGameFailure") delayTime:1.5 isHappy:NO];
+                                }
+                            }];
+    
+    [self registerNotificationWithName:NOTIFICATION_NETWORK_CONNECTED
+                            usingBlock:^(NSNotification *note) {
+                                [self didConnected];
+                            }];
+    
+    //    [self registerNotificationWithName:UIApplicationWillEnterForegroundNotification
+    //                            usingBlock:^(NSNotification *note) {
+    //        PPDebug(@"<DiceRoomListController> Disconnected from server");
+    //        if (![[DiceGameService defaultService] isConnected]) {
+    //            [self didBroken];
+    //        }
+    //    }];
+    
+}
+
+- (void)unregisterDiceRoomNotification
+{
+    [self unregisterAllNotifications];
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    _isRefreshing = YES;
+    [self updateOnlineUserCount];
+    //    [[NSNotificationCenter defaultCenter] addObserver:self
+    //                                             selector:@selector(roomsDidUpdate:)
+    //                                                 name:ROOMS_DID_UPDATE
+    //                                               object:nil];
+}
+
+- (void)viewDidUnload
+{
+
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [self registerDiceRoomNotification];
+    [super viewDidAppear:animated];
+    [self connectServer];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [self unregisterDiceRoomNotification];
+    [_searchView disappear];
+    [self clearRefreshRoomsTimer];
+    [super viewDidDisappear:animated];
+}
+
+#pragma mark - TableView delegate methods
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return [CommonRoomListCell getCellHeight];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    CommonRoomListCell *cell = [tableView dequeueReusableCellWithIdentifier:[CommonRoomListCell getCellIdentifier]];
+    if (cell == nil) {
+        cell = [CommonRoomListCell createCell:[CommonRoomListCell getCellIdentifier]];
+    }
+    cell.delegate = self;
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _gameService.roomList.count;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    self.currentSession = [_gameService sessionInRoom:indexPath.row];
+    if (self.currentSession.password == nil
+        || self.currentSession.password.length <= 0
+        || [[UserManager defaultManager] isMe:self.currentSession.createBy])
+    {
+        [self checkAndJoinGame:self.currentSession.sessionId];
+    } else {
+        InputDialog *inputDialog = [InputDialog dialogWith:NSLS(@"kPassword")
+                                                  delegate:self
+                                                     theme:CommonDialogThemeDice];
+        inputDialog.targetTextField.text = nil;
+        inputDialog.targetTextField.placeholder = NSLS(@"kEnterPassword");
+        [inputDialog showInView:self.view];
+        inputDialog.tag = ENTER_ROOM_DIALOG_TAG;
+    }
+    
+    
+}
+
+#pragma mark - Button action
+
+- (IBAction)clickBackButton:(id)sender {
+    [_gameService quitGame];
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (IBAction)clickFastEntryButton:(id)sender {
+    [self checkAndJoinGame];
+}
+
+- (IBAction)creatRoom:(id)sender
+{
+    PPDebug(@"<CommonRoomListController> creatRoom method not implement");
+}
+
+- (void)showCreateRoomView
+{ 
+    RoomPasswordDialog *inputDialog = [RoomPasswordDialog dialogWith:NSLS(@"kCreateRoom")
+                                                            delegate:self
+                                                               theme:CommonDialogThemeDice];
+    inputDialog.targetTextField.text = [[UserManager defaultManager] defaultUserRoomName];
+    inputDialog.targetTextField.placeholder = NSLS(@"kInputWordPlaceholder");
+    inputDialog.passwordField.placeholder = NSLS(@"kDiceEnterPassword");
+    [inputDialog showInView:self.view];
+    inputDialog.tag = CREATE_ROOM_DIALOG_TAG;
+}
+
+
+
+- (void)refreshRoomsByFilter:(RoomFilter)filter
+{
+    [_gameService getRoomList:0
+                                            count:ROOMS_COUNT_PER_PAGE
+                                         roomType:filter
+                                          keyword:nil
+                                           gameId:[ConfigManager gameId]];
+}
+
+
+
+- (void)updateOnlineUserCount
+{
+
+}
+
+#pragma mark - CommonGameServiceDelegate
+- (void)didConnected
+{
+    [self hideActivity];
+    [_gameService getRoomList:0 count:ROOMS_COUNT_PER_PAGE];
+    firstLoad = NO;
+}
+
+- (void)didBroken
+{
+    PPDebug(@"%@ <didBroken>", [self description]);
+    [self hideActivity];
+    
+    //    [self popupUnhappyMessage:NSLS(@"kNetworkBroken") title:@""];
+    [self.navigationController popToRootViewControllerAnimated:NO];
+    
+}
+
+#pragma makr - inputDialog delegate
+- (void)didClickOk:(InputDialog *)dialog
+        targetText:(NSString *)targetText
+{
+    if (dialog.tag == CREATE_ROOM_DIALOG_TAG) {
+        NSString *password = ((RoomPasswordDialog *)dialog).passwordField.text;
+        [self createRoomWithName:targetText
+                        password:password];
+    }
+    
+    if (dialog.tag == ENTER_ROOM_DIALOG_TAG) {
+        if ([self.currentSession.password isEqualToString:targetText]) {
+            [self checkAndJoinGame:self.currentSession.sessionId];
+        } else {
+            [self popupMessage:NSLS(@"kPsdNotMatch") title:nil];
+        }
+    }
+}
+- (void)didClickCancel:(InputDialog *)dialog
+{
+    
+}
+
+#pragma mark - load more delegate
+- (void)loadMoreTableViewDataSource
+{
+    [_gameService getRoomList:_gameService.roomList.count count:ROOMS_COUNT_PER_PAGE];
+}
+
+#pragma mark - common dialog delegate
+- (void)clickOk:(CommonDialog *)dialog
+{
+    if ([ConfigManager wallEnabled]) {
+        [self showWall];
+    }else {
+        CoinShopController* controller = [[[CoinShopController alloc] init] autorelease];
+        [self.navigationController pushViewController:controller animated:YES];
+    }
+}
+
+- (void)clickBack:(CommonDialog *)dialog
+{
+    
+}
+
+- (void)showWall
+{
+    [UIUtils alertWithTitle:@"免费金币获取提示" msg:@"下载免费应用即可获取金币！下载完应用一定要打开才可以获得奖励哦！"];
+    [[LmWallService defaultService] show:self];
+}
+
+#pragma mark - common info view delegate
+- (void)infoViewDidDisappear:(CommonInfoView*)view
+{
+    _searchView = nil;
+    if (_currentRoomType == allRoom) {
+        [self continueRefreshingRooms];
+    }
+    if (_currentRoomType == friendRoom) {
+//        [self clickFriendRoom:nil];
+    }
+    
+}
+
+#pragma mark - CommonSearchViewDelegate
+
+- (void)willSearch:(NSString *)keywords byView:(CommonSearchView *)view
+{
+    [_gameService getRoomList:0
+                                            count:ROOMS_COUNT_PER_PAGE
+                                         roomType:_currentRoomType
+                                          keyword:keywords
+                                           gameId:[ConfigManager gameId]];
+    [self showActivityWithText:NSLS(@"kSearchingRoom")];
+    [self pauseRefreshingRooms];
+}
+
+- (CGPoint)searchViewPoint
+{
+    PPDebug(@"<CommonRoomListController> searchViewPoint method not implement");
+    return CGPointMake(0, 0);
+}
+
+- (IBAction)clickSearch:(id)sender
+{
+    if (_searchView) {
+        [_searchView disappear];
+    } else {
+        _searchView = [CommonSearchView showInView:self.view
+                                           byTheme:CommonSearchViewThemeDice
+                                           atPoint:[self searchViewPoint]
+                                          delegate:self];
+    }
+}
+
+#pragma mark - dice room list delegate
+- (void)didQueryUser:(NSString *)userId
+{
+
+}
+
 
 @end
