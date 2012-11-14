5b5d123e8ab0ebb8ac3a9537569e90a23add1478
GamyHuang <hguangm2009@gmail.com>
2012-11-14 17:09:52 +0800

    add bbs related files.

---
+++ Draw/BBS/Controller/BBSBoardController.h
@@ -0,0 +1,13 @@
+//
+//  BBSBoardController.h
+//  Draw
+//
+//  Created by gamy on 12-11-14.
+//
+//
+
+#import "PPTableViewController.h"
+
+@interface BBSBoardController : PPTableViewController
+
+@end
