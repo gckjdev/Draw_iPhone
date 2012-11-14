f54eda610ec1cbda9f361da9e771cf541f8b505b
kira <ouyongyong@163.com>
2012-11-14 10:24:29 +0800

    add common room list

---
+++ Draw/Controller/Common/CommonRoomListController.h
@@ -0,0 +1,13 @@
+//
+//  CommonRoomListController.h
+//  Draw
+//
+//  Created by Kira on 12-11-14.
+//
+//
+
+#import "PPTableViewController.h"
+
+@interface CommonRoomListController : PPTableViewController
+
+@end
---
+++ Draw/Controller/Common/CommonRoomListController.m
@@ -0,0 +1,38 @@
+//
+//  CommonRoomListController.m
+//  Draw
+//
+//  Created by Kira on 12-11-14.
+//
+//
+
+#import "CommonRoomListController.h"
+
+@interface CommonRoomListController ()
+
+@end
+
+@implementation CommonRoomListController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
