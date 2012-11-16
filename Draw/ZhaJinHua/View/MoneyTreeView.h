6498928a6d1d18e84698548295748efdea50b7ce
kira <ouyongyong@163.com>
2012-11-16 10:43:41 +0800

    update

---
+++ Draw/ZhaJinHua/View/MoneyTreeView.h
@@ -0,0 +1,13 @@
+//
+//  MoneyTreeView.h
+//  Draw
+//
+//  Created by Kira on 12-11-16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoneyTreeView : UIView
+
+@end
---
+++ Draw/ZhaJinHua/View/MoneyTreeView.m
@@ -0,0 +1,31 @@
+//
+//  MoneyTreeView.m
+//  Draw
+//
+//  Created by Kira on 12-11-16.
+//
+//
+
+#import "MoneyTreeView.h"
+
+@implementation MoneyTreeView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
