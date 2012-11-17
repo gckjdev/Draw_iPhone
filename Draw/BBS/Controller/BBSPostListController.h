daa57bbdfd20eca24de3ee5ee00d2cb17c03ca41
GamyHuang <hguangm2009@gmail.com>
2012-11-17 20:35:18 +0800

    update contest

--- Draw/BBS/Controller/BBSPostListController.h
+++ Draw/BBS/Controller/BBSPostListController.h
@@ -8,6 +8,21 @@
 
 #import "CommonTabController.h"
 
+
+@class PBBBSPost;
+@class PBBBSUser;
+@class PBBBSBoard;
+
 @interface BBSPostListController : CommonTabController
+{
+    
+}
+
+//@property (retain, nonatomic)  UIButton *backButton;
+@property (retain, nonatomic) IBOutlet UIButton *backButton;
+@property (retain, nonatomic) IBOutlet UIButton *createPostButton;
+@property (retain, nonatomic) IBOutlet UIButton *rankButton;
+- (IBAction)clickCreatePostButton:(id)sender;
+- (IBAction)clickRankButton:(id)sender;
 
 @end
