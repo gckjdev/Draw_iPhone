22aac6beb387b320b25c48a97e20c4cb385a4ef5
GamyHuang <hguangm2009@gmail.com>
2012-11-17 16:00:21 +0800

    update post list and detail

---
+++ Draw/BBS/Controller/BBSPostDetailController.h
@@ -0,0 +1,13 @@
+//
+//  BBSPostDetailController.h
+//  Draw
+//
+//  Created by gamy on 12-11-17.
+//
+//
+
+#import "CommonTabController.h"
+
+@interface BBSPostDetailController : CommonTabController
+
+@end
