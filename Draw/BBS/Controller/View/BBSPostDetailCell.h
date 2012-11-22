7e961083a2b70db31a97a528df1aaa20272beb82
GamyHuang <hguangm2009@gmail.com>
2012-11-22 18:18:51 +0800

    finish post detail controller

--- Draw/BBS/Controller/View/BBSPostDetailCell.h
+++ Draw/BBS/Controller/View/BBSPostDetailCell.h
@@ -13,11 +13,13 @@
 
 @property (retain, nonatomic) IBOutlet UILabel *textContent;
 @property (retain, nonatomic) IBOutlet UIImageView *imageContent;
+@property (retain, nonatomic) IBOutlet UILabel *time;
 
 
 + (BBSPostDetailCell *)createCell:(id)delegate;
 + (NSString*)getCellIdentifier;
 + (CGFloat)getCellHeight;
++ (CGFloat)getCellHeightWithPost:(PBBBSPost *)post;
 - (void)updateCellWithPost:(PBBBSPost *)post;
 
 @end
