09501ebb5bd0e4c91785fb44342184ce6bdcc6e2
kira <ouyongyong@163.com>
2012-11-27 13:21:55 +0800

    add starry loading view

---
+++ Draw/Controller/Common/PPViewController+StarryLoadingView.h
@@ -0,0 +1,13 @@
+//
+//  PPViewController+StarryLoadingView.h
+//  Draw
+//
+//  Created by Kira on 12-11-27.
+//
+//
+
+#import "PPViewController.h"
+
+@interface PPViewController (StarryLoadingView)
+
+@end
---
+++ Draw/Controller/Common/PPViewController+StarryLoadingView.m
@@ -0,0 +1,13 @@
+//
+//  PPViewController+StarryLoadingView.m
+//  Draw
+//
+//  Created by Kira on 12-11-27.
+//
+//
+
+#import "PPViewController+StarryLoadingView.h"
+
+@implementation PPViewController (StarryLoadingView)
+
+@end
