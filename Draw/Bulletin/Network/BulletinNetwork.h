8ca98382c550876b01711dc5cb25f3b7e611a12f
kira <ouyongyong@163.com>
2012-12-17 17:39:18 +0800

    add bulletin network

---
+++ Draw/Bulletin/Network/BulletinNetwork.h
@@ -0,0 +1,13 @@
+//
+//  BulletinNetwork.h
+//  Draw
+//
+//  Created by Kira on 12-12-17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BulletinNetwork : NSObject
+
+@end
---
+++ Draw/Bulletin/Network/BulletinNetwork.m
@@ -0,0 +1,13 @@
+//
+//  BulletinNetwork.m
+//  Draw
+//
+//  Created by Kira on 12-12-17.
+//
+//
+
+#import "BulletinNetwork.h"
+
+@implementation BulletinNetwork
+
+@end
