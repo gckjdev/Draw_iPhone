ecbddcf924e4f54dab39650133d10d6cf3426c1c
kira <ouyongyong@163.com>
2012-12-17 18:18:38 +0800

    add bulletin network constants

---
+++ Draw/Bulletin/Network/BulletinNetworkConstants.h
@@ -0,0 +1,14 @@
+//
+//  BulletinNetworkConstants.h
+//  Draw
+//
+//  Created by Kira on 12-12-17.
+//
+//
+
+#ifndef Draw_BulletinNetworkConstants_h
+#define Draw_BulletinNetworkConstants_h
+
+
+
+#endif
