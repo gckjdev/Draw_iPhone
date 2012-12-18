dd445faffe68a79c9604e6dfe5f6b7311e4bd7a0
kira <ouyongyong@163.com>
2012-12-18 09:23:21 +0800

    add bulletin manager

---
+++ Draw/Bulletin/Model/BulletinManager.h
@@ -0,0 +1,13 @@
+//
+//  BulletinManager.h
+//  Draw
+//
+//  Created by Kira on 12-12-17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BulletinManager : NSObject
+
+@end
---
+++ Draw/Bulletin/Model/BulletinManager.m
@@ -0,0 +1,13 @@
+//
+//  BulletinManager.m
+//  Draw
+//
+//  Created by Kira on 12-12-17.
+//
+//
+
+#import "BulletinManager.h"
+
+@implementation BulletinManager
+
+@end
