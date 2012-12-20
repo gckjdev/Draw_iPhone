487b708c446c4f7349c7af8a174d3c519d680f55
GamyHuang <hguangm2009@gmail.com>
2012-12-20 16:14:14 +0800

    add dice and zhajinhua target

--- Draw/DrawAppDelegate.m
+++ Draw/DrawAppDelegate.m
@@ -73,7 +73,7 @@ NSString* GlobalGetServerURL()
 NSString* GlobalGetTrafficServerURL()
 {
     return [ConfigManager getTrafficAPIServerURL];
-//    return @"http://192.168.1.9:8100/api/i?";
+//    return @"http://192.168.1.7:8100/api/i?";
 //    return @"http://192.168.1.198:8100/api/i?";
 }
 
