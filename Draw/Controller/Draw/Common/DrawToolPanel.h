bef3dc47da89efa924322dfd49a42e4773a83d0a
GamyHuang <hguangm2009@gmail.com>
2012-12-24 15:53:38 +0800

    add draw tool panel

---
+++ Draw/Controller/Draw/Common/DrawToolPanel.h
@@ -0,0 +1,13 @@
+//
+//  DrawToolPanel.h
+//  Draw
+//
+//  Created by gamy on 12-12-24.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrawToolPanel : UIView
+
+@end
---
+++ Draw/Controller/Draw/Common/DrawToolPanel.m
@@ -0,0 +1,31 @@
+//
+//  DrawToolPanel.m
+//  Draw
+//
+//  Created by gamy on 12-12-24.
+//
+//
+
+#import "DrawToolPanel.h"
+
+@implementation DrawToolPanel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
