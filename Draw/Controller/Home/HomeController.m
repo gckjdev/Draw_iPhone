64763debf633af0a108dfa2f2dbf43341439f79c
GamyHuang <hguangm2009@gmail.com>
2012-12-29 11:27:17 +0800

    add commint shell script

--- Draw/Controller/Home/HomeController.m
+++ Draw/Controller/Home/HomeController.m
@@ -205,6 +205,7 @@
 //    }
 
     [self enterNextControllerWityType:self.notificationType];
+
 //     [self.view bringSubviewToFront:self.testBulletin];
 
 //    [DrawTestViewController enterWithController:self];
---
+++ commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo 'add files'
+git add *.h *.m *.xib
+
+echo 'checkout unuse files'
+git checkout *.xcuserstate *DS_Store
+
+echo 'commit files'
+git commit -m "$1"
+
+echo 'pull'
+git pull
+
+echo 'push'
+git push
