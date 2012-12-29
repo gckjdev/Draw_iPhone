c474e290c048d519e39c65ab8fd58edc0646c787
Benson Peng <penglingzhe@gmail.com>
2012-12-29 12:02:44 +0800

    update commit shell for display

--- commit.sh
+++ commit.sh
@@ -3,14 +3,19 @@
 echo '=========== add files (*.h *.m *.xib *.strings *.plist *.png *.jpg *.txt *.sh *.pbxproj)  ====================='
 git add *.h *.m *.xib *.strings *.plist *.png *.jpg *.txt *.sh *.pbxproj
 
-echo '=========== auto checkout unuse files (*.xcuserstate *DS_Store)  ==================='
+echo '=========== auto checkout unuse files (*.xcuserstate *DS_Store)  ====='
 git checkout *.xcuserstate *DS_Store
+echo '\r\n'
 
-echo '========== commit files. Comment:'"$1""==================="
+echo '========== oh, commit files. Comment:'"$1""==========================="
 git commit -m "$1"
+echo '\r\n'
 
-echo '========== pull code from server ========================='
+echo '========== hey, pull code from server ================================'
 git pull
+echo '\r\n'
 
-echo '========== push code to server ==========================='
+echo '========== wow, push code to server =================================='
 git push
+echo '========== congratulations! hope there is no conflict! ==============='
+
