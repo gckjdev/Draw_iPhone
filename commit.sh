599d7d129dcf45e1964468d957801cafb943ccc7
Benson Peng <penglingzhe@gmail.com>
2012-12-29 12:04:19 +0800

    update commit shell for display

--- commit.sh
+++ commit.sh
@@ -2,6 +2,7 @@
 
 echo '=========== add files (*.h *.m *.xib *.strings *.plist *.png *.jpg *.txt *.sh *.pbxproj)  ====================='
 git add *.h *.m *.xib *.strings *.plist *.png *.jpg *.txt *.sh *.pbxproj
+echo ''
 
 echo '=========== auto checkout unuse files (*.xcuserstate *DS_Store)  ====='
 git checkout *.xcuserstate *DS_Store
