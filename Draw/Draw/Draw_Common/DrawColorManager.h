6b7ad5c6e9a2e3862ff92dc2812d5cf9c2de67ff
GamyHuang <hguangm2009@gmail.com>
2013-01-05 10:28:10 +0800

    update project: add files

---
+++ Draw/Draw/Draw_Common/DrawColorManager.h
@@ -0,0 +1,14 @@
+//
+//  DrawColorManager.h
+//  Draw
+//
+//  Created by gamy on 13-1-5.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "DrawColor.h"
+
+@interface DrawColorManager : NSObject
+
+@end
---
+++ Draw/Draw/Draw_Common/DrawColorManager.m
@@ -0,0 +1,13 @@
+//
+//  DrawColorManager.m
+//  Draw
+//
+//  Created by gamy on 13-1-5.
+//
+//
+
+#import "DrawColorManager.h"
+
+@implementation DrawColorManager
+
+@end
