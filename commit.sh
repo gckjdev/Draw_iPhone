14b5086cee9671e6594deaf799d6b5eba3c01941
GamyHuang <hguangm2009@gmail.com>
2013-01-07 12:17:38 +0800

    update pens.

--- commit.sh
+++ commit.sh
@@ -12,7 +12,9 @@ git add *.proto *.h *.m *.xib *.strings *.plist *.png *.jpg *.txt *.sh *.pbxproj
 echo ''
 
 echo $line' auto checkout unuse files (*xcbkptlist *xcuserstate *DS_Store)  '$line
-git checkout *xcbkptlist *xcuserstate *DS_Store
+git checkout *xcbkptlist 
+git checkout *xcuserstate 
+git checkout *DS_Store
 echo ''
 
 echo $line' oh, commit files. comment: '"$1"" "$line
