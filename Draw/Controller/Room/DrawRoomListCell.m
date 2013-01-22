5e3421cf468c680f9f46c397b05a75a08aa5b45d
kira <ouyongyong@163.com>
2013-01-22 10:14:12 +0800

    change proj , add draw room list

---
+++ Draw/Controller/Room/DrawRoomListCell.m
@@ -0,0 +1,31 @@
+//
+//  DrawRoomListCell.m
+//  Draw
+//
+//  Created by Kira on 13-1-22.
+//
+//
+
+#import "DrawRoomListCell.h"
+
+@implementation DrawRoomListCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ Draw/Controller/Room/DrawRoomListController.m
@@ -0,0 +1,38 @@
+//
+//  DrawRoomListController.m
+//  Draw
+//
+//  Created by Kira on 13-1-22.
+//
+//
+
+#import "DrawRoomListController.h"
+
+@interface DrawRoomListController ()
+
+@end
+
+@implementation DrawRoomListController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
