ea103b25bb127ecf315840a09bc0c2a75cdae115
GamyHuang <hguangm2009@gmail.com>
2013-01-26 12:12:53 +0800

    bug fix

--- Draw/Controller/Feed/Cell/DrawInfoCell.m
+++ Draw/Controller/Feed/Cell/DrawInfoCell.m
@@ -211,7 +211,7 @@
 {    
     [self setFeed:feed];
     [self updateTime:self.feed];
-    [self updateDrawToUserInfo:feed];
+//    [self updateDrawToUserInfo:feed];
     [self updateDesc:feed.opusDesc];
     [self.loadingActivity setCenter:self.drawImage.center];
     if ([feed hasDrawActions]) {
