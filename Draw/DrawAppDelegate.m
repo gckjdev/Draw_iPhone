552e714054d2524fd0747974adf99fa59cc9a703
GamyHuang <hguangm2009@gmail.com>
2013-02-01 16:07:14 +0800

    adjust the frame

--- Draw/DrawAppDelegate.m
+++ Draw/DrawAppDelegate.m
@@ -75,8 +75,8 @@ NSString* GlobalGetServerURL()
 
 NSString* GlobalGetTrafficServerURL()
 {
-//    return [ConfigManager getTrafficAPIServerURL];
-    return @"http://192.168.1.198:8100/api/i?";
+    return [ConfigManager getTrafficAPIServerURL];
+//    return @"http://192.168.1.198:8100/api/i?";
 //    return @"http://192.168.1.5:8100/api/i?";
 //    return @"http://192.168.1.123:8100/api/i?";
 }
