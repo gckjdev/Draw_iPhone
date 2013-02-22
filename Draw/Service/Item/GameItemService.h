1232598c73668cd4e37b1ae98f1aef715029d66b
Benson Peng <penglingzhe@gmail.com>
2013-02-22 12:04:29 +0800

    add game item model and service

---
+++ Draw/Service/Item/GameItemService.h
@@ -0,0 +1,13 @@
+//
+//  GameItemService.h
+//  Draw
+//
+//  Created by qqn_pipi on 13-2-22.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameItemService : NSObject
+
+@end
---
+++ Draw/Service/Item/UserGameItemService.m
@@ -0,0 +1,13 @@
+//
+//  UserGameItemService.m
+//  Draw
+//
+//  Created by qqn_pipi on 13-2-22.
+//
+//
+
+#import "UserGameItemService.h"
+
+@implementation UserGameItemService
+
+@end
