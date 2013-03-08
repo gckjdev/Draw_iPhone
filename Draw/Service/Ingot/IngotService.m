61300cfcbde759266eb32a2871320091c166f5fe
haodong <qiuhaodong2006@126.com>
2013-03-08 12:33:55 +0800

    update ChargeController

--- Draw/Service/Ingot/IngotService.m
+++ Draw/Service/Ingot/IngotService.m
@@ -38,48 +38,34 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(IngotService);
 
 
 //************************************************************
-//message PBSaleIngot{
-//    required int32 count = 1;                 // 个数
-//    optional string totalPrice = 2;           // 总价格
-//    optional string currency = 3;             // 货币符号
-//    optional string country = 5;              // 适应国家，中国"CN"
-//}
-
-
 + (void)createTestDataFile
 {
     NSMutableArray *mutableArray = [[[NSMutableArray alloc] init] autorelease];
     
-    for (int index = 0 ; index < 6; index ++) {
+    for (int index = 0 ; index < 4; index ++) {
         
         PBSaleIngot_Builder *saleIngot_Builder = [[PBSaleIngot_Builder alloc] init];
         
         if (index == 0) {
-            [saleIngot_Builder setCount:6];
-            [saleIngot_Builder setTotalPrice:@"6"];
-            [saleIngot_Builder setCurrency:@"￥"];
+            [saleIngot_Builder setCount:8];
+            [saleIngot_Builder setTotalPrice:@"8"];
         } else if (index == 1){
-            [saleIngot_Builder setCount:13];
-            [saleIngot_Builder setTotalPrice:@"12"];
-            [saleIngot_Builder setCurrency:@"￥"];
-        } else if (index == 2){
             [saleIngot_Builder setCount:20];
             [saleIngot_Builder setTotalPrice:@"18"];
-            [saleIngot_Builder setCurrency:@"￥"];
+            [saleIngot_Builder setSaving:@"10%"];
+        } else if (index == 2){
+            [saleIngot_Builder setCount:50];
+            [saleIngot_Builder setTotalPrice:@"45"];
+            [saleIngot_Builder setSaving:@"10%"];
         } else if (index == 3){
-            [saleIngot_Builder setCount:65];
-            [saleIngot_Builder setTotalPrice:@"60"];
-            [saleIngot_Builder setCurrency:@"￥"];
-        } else if (index == 4){
-            [saleIngot_Builder setCount:132];
-            [saleIngot_Builder setTotalPrice:@"120"];
-            [saleIngot_Builder setCurrency:@"￥"];
-        } else if (index == 5){
-            [saleIngot_Builder setCount:276];
-            [saleIngot_Builder setTotalPrice:@"240"];
-            [saleIngot_Builder setCurrency:@"￥"];
+            [saleIngot_Builder setCount:100];
+            [saleIngot_Builder setTotalPrice:@"80"];
+            [saleIngot_Builder setSaving:@"20%"];
         }
-    
+        
+        [saleIngot_Builder setCurrency:@"￥"];
+        [saleIngot_Builder setCountry:@"CN"];
+        
         PBSaleIngot *saleIngot = [saleIngot_Builder build];
         [saleIngot_Builder release];
         [mutableArray addObject:saleIngot];
