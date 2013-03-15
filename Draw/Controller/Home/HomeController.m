1f8c5e8d4cca61f4c2174cd44c368580bbddb313
kira <ouyongyong@163.com>
2013-03-15 10:30:50 +0800

    update

--- Draw/Controller/Home/HomeController.m
+++ Draw/Controller/Home/HomeController.m
@@ -666,8 +666,8 @@
         case HomeMenuTypeDiceFreeCoins:
         {
             [[AnalyticsManager sharedAnalyticsManager] reportClickHomeMenu:HOME_ACTION_FREE_COINS];
-            FreeIngotController* fc = [[[FreeIngotController alloc] init] autorelease];
-            [self.navigationController pushViewController:fc animated:YES];
+            FreeCoinsControllerViewController *vc = [[[FreeCoinsControllerViewController alloc] init] autorelease];
+            [self.navigationController pushViewController:vc animated:YES];
 //            [UIUtils alertWithTitle:@"免费金币获取提示" msg:@"下载免费应用即可获取金币！下载完应用一定要打开才可以获得奖励哦！"];
 //            [[LmWallService defaultService] show:self];
         }
@@ -676,9 +676,9 @@
         case HomeMenuTypeDrawFreeCoins:
         {
             [[AnalyticsManager sharedAnalyticsManager] reportClickHomeMenu:HOME_ACTION_FREE_COINS];
-
-            FreeCoinsControllerViewController *vc = [[[FreeCoinsControllerViewController alloc] init] autorelease];
-            [self.navigationController pushViewController:vc animated:YES];
+            FreeIngotController* fc = [[[FreeIngotController alloc] init] autorelease];
+            [self.navigationController pushViewController:fc animated:YES];
+            
         }
             break;
             
