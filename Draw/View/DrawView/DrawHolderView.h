838f1f3f52a9c2bba08eafbabb4ccd7d900b97c9
GamyHuang <hguangm2009@gmail.com>
2013-03-15 14:46:26 +0800

    add files

---
+++ Draw/View/DrawView/DrawHolderView.h
@@ -0,0 +1,13 @@
+//
+//  DrawHolderView.h
+//  Draw
+//
+//  Created by gamy on 13-3-15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DrawHolderView : NSObject
+
+@end
---
+++ Draw/View/DrawView/DrawHolderView.m
@@ -0,0 +1,13 @@
+//
+//  DrawHolderView.m
+//  Draw
+//
+//  Created by gamy on 13-3-15.
+//
+//
+
+#import "DrawHolderView.h"
+
+@implementation DrawHolderView
+
+@end
