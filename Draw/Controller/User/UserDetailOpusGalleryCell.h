79255dccf1bdaa96ec23c2d5875e92d92950e644
kira <ouyongyong@163.com>
2013-03-18 17:59:07 +0800

    add user detail controller

---
+++ Draw/Controller/User/UserDetailOpusGalleryCell.h
@@ -0,0 +1,13 @@
+//
+//  UserDetailOpusGalleryCell.h
+//  Draw
+//
+//  Created by Kira on 13-3-18.
+//
+//
+
+#import "PPTableViewCell.h"
+
+@interface UserDetailOpusGalleryCell : PPTableViewCell
+
+@end
