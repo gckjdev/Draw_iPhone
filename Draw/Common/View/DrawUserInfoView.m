37419d95fcc8d5cc6fedcad5f4c07d1791531ca0
kira <ouyongyong@163.com>
2013-03-23 15:25:21 +0800

    update

--- Draw/Common/View/DrawUserInfoView.m
+++ Draw/Common/View/DrawUserInfoView.m
@@ -409,7 +409,7 @@
         infoInView:(PPViewController*)superController
         needUpdate:(BOOL)needUpdate //if need update the info from service.
 {
-    if ([[UserManager defaultManager] isMe: friend.friendUserId]) {
+    if ([[UserManager defaultManager] isMe:afriend.friendUserId]) {
         [superController popupMessage:NSLS(@"kCannotShowYourself") title:nil];
         return;
     }
