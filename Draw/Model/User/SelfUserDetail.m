f165fe916396c10671b0108a08efa6694af50f22
Benson Peng <penglingzhe@gmail.com>
2013-03-28 17:12:30 +0800

    update finish block

--- Draw/Model/User/SelfUserDetail.m
+++ Draw/Model/User/SelfUserDetail.m
@@ -19,7 +19,7 @@
 #import "ConfigManager.h"
 
 @interface SelfUserDetail() {
-    LoadFeedFinishBlock _finishBlock;
+    
 }
 
 @property (nonatomic, assign) PPTableViewController* superViewController;
@@ -27,6 +27,7 @@
 @property (retain, nonatomic) NSMutableArray* opusList;
 @property (retain, nonatomic) NSMutableArray* guessedList;
 @property (retain, nonatomic) NSMutableArray* favouriateList;
+@property (copy, nonatomic)   LoadFeedFinishBlock finishBlock;
 
 @end
 
@@ -47,6 +48,7 @@
 
 - (void)dealloc
 {
+    self.finishBlock = nil;
     PPRelease(_superViewController);
     PPRelease(_opusList);
     PPRelease(_guessedList);
@@ -151,6 +153,8 @@
 
 - (void)loadFeedByTabAction:(int)tabAction finishBLock:(LoadFeedFinishBlock)block
 {
+    self.finishBlock = block;
+    
     switch (tabAction) {
         case DetailTabActionClickFavouriate: {
             
@@ -164,8 +168,10 @@
         default:
             break;
     }
-    RELEASE_BLOCK(_finishBlock);
-    COPY_BLOCK(_finishBlock, block);
+    
+    
+//    RELEASE_BLOCK(_finishBlock);
+//    COPY_BLOCK(_finishBlock, block);
 }
 
 - (void)blackUser:(PPTableViewController *)viewController
@@ -268,6 +274,7 @@
                     }
                 }
                 EXECUTE_BLOCK(_finishBlock, resultCode, self.guessedList);
+                self.finishBlock = nil;
 //                [[self detailCell] setDrawFeedList:self.guessedList];
             } break;
             case FeedListTypeUserOpus: {
@@ -281,13 +288,20 @@
 //                [cell setDrawFeedList:self.opusList];
                 
                 EXECUTE_BLOCK(_finishBlock, resultCode, self.opusList);
+                self.finishBlock = nil;
             }
-            default:
+            default:{
+                EXECUTE_BLOCK(_finishBlock, resultCode, self.opusList);
+                self.finishBlock = nil;
                 break;
+            }
         }
     } else {
         EXECUTE_BLOCK(_finishBlock, resultCode, nil);
+        self.finishBlock = nil;
     }
+    
+    
 }
 
 - (NSString*)blackUserBtnTitle
