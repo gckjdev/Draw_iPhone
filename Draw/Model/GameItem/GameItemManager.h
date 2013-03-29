77be777615e6eb9a67134a22dd3d982843380aad
xiaotaowang <445242970@qq.com>
2013-03-29 11:12:51 +0800

    add pb files

--- Draw/Model/GameItem/GameItemManager.h
+++ Draw/Model/GameItem/GameItemManager.h
@@ -11,15 +11,13 @@
 
 @class PBGameItem;
 
-#define SHOP_ITEMS_FILE_WITHOUT_SUFFIX @"shop_item"
-#define SHOP_ITEM_FILE_TYPE @"pb"
-#define SHOP_ITEMS_FILE @"shop_item.pb"
-#define SHOP_ITEMS_FILE_BUNDLE_PATH @"shop_item.pb"
-#define SHOP_ITEMS_FILE_VERSION @"1.0"
 
 @interface GameItemManager : NSObject
 
 + (GameItemManager *)defaultManager;
++ (NSString *)shopItemsFileName;
++ (NSString *)shopItemsFileBundlePath;
++ (NSString *)shopItemsFileVersion;
 
 - (void)setItemsList:(NSArray *)itemsList;
 - (NSArray *)itemsList;
