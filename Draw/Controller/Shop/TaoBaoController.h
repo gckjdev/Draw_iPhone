bfc702fc7f0d35b23e440b2fc2530d08119f14a6
haodong <qiuhaodong2006@126.com>
2013-04-01 11:52:16 +0800

    add TaoBaoController

---
+++ Draw/Controller/Shop/TaoBaoController.h
@@ -0,0 +1,16 @@
+//
+//  TaoBaoController.h
+//  Draw
+//
+//  Created by haodong on 13-4-1.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "PPViewController.h"
+
+@interface TaoBaoController : PPViewController <UIWebViewDelegate>
+
+@property (retain, nonatomic) IBOutlet UIWebView *taoBaoWebView;
+
+@end
