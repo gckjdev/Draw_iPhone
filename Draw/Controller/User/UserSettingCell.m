1989a06f4fbaea34497999026ee0bb6ca24a54a3
kira <ouyongyong@163.com>
2013-04-11 11:03:42 +0800

    add user setting cell

---
+++ Draw/Controller/User/UserSettingCell.m
@@ -0,0 +1,31 @@
+//
+//  UserSettingCell.m
+//  Draw
+//
+//  Created by Kira on 13-4-11.
+//
+//
+
+#import "UserSettingCell.h"
+
+@implementation UserSettingCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
