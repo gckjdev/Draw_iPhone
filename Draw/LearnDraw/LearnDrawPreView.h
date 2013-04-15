55274de13864a4b4d2465d70cd97b046a67281b8
GamyHuang <hguangm2009@gmail.com>
2013-04-15 10:27:56 +0800

    add learn draw pre view && adjust the diretories

---
+++ Draw/LearnDraw/LearnDrawPreView.h
@@ -0,0 +1,13 @@
+//
+//  LearnDrawPreView.h
+//  Draw
+//
+//  Created by gamy on 13-4-15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LearnDrawPreView : NSObject
+
+@end
---
+++ Draw/LearnDraw/LearnDrawPreView.m
@@ -0,0 +1,13 @@
+//
+//  LearnDrawPreView.m
+//  Draw
+//
+//  Created by gamy on 13-4-15.
+//
+//
+
+#import "LearnDrawPreView.h"
+
+@implementation LearnDrawPreView
+
+@end
