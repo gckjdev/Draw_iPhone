022d50de2c308ede00748ae82d2252a46a84e7ee
Benson Peng <penglingzhe@gmail.com>
2013-04-29 12:41:43 +0800

    fix sk product crash

--- Draw/Model/GameItem/SKProductManager.m
+++ Draw/Model/GameItem/SKProductManager.m
@@ -28,8 +28,7 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(SKProductManager);
 - (id)init
 {
     if (self = [super init]) {
-        NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-        self.productList = [userDefaults objectForKey:KEY_SKPRODUCT_LIST];
+
     }
     
     return self;
@@ -44,20 +43,6 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(SKProductManager);
         }
     }
     return nil;
-}
-
-- (void)setProductList:(NSArray *)productList
-{
-    if ([productList count] <= 0) {
-        return;
-    }
-    
-    [_productList release];
-    _productList = [productList retain];
-    
-    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-    [userDefaults setObject:[self.productList data]forKey:KEY_SKPRODUCT_LIST];
-    [userDefaults synchronize];
 }
 
 
