74051718ccf9b5aa2e6af027303d1b53a6484503
Benson Peng <penglingzhe@gmail.com>
2013-04-29 21:20:57 +0800

    submit 2 new apps

--- Draw/DrawAppDelegate.m
+++ Draw/DrawAppDelegate.m
@@ -299,14 +299,7 @@ NSString* GlobalGetBoardServerURL()
     
     [self.window makeKeyAndVisible];
     
-//    [[SKProductService defaultService] syncDataFromIAPService];
-    
-    // Fetch Server List At Background
-//    if (isDrawApp() == NO && isLearnDrawApp() == NO){
-//        // no longer used for Draw App
-//        // TODO can be removed after ZJH and DICE is used new item & shop design PB files
-//        [[PriceService defaultService] syncShoppingListAtBackground];
-//    }
+    [[SKProductService defaultService] syncDataFromIAPService];
     
     [[AccountService defaultService] retryVerifyReceiptAtBackground];
     
@@ -387,7 +380,7 @@ NSString* GlobalGetBoardServerURL()
     // load item data
     [[GameItemService defaultService] syncData:NULL];
     [[IAPProductService defaultService] syncData:NULL];
-    
+        
     if ([GameApp isAutoRegister]){
         [[UserService defaultService] autoRegisteration:nil];
     }
