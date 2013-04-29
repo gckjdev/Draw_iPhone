b2a93bd5cb58f06336acecc201ca1c369f869e9d
xiaotaowang <445242970@qq.com>
2013-04-29 11:44:49 +0800

    commit

--- Draw/Model/GameItem/SKProductManager.m
+++ Draw/Model/GameItem/SKProductManager.m
@@ -8,6 +8,9 @@
 
 #import "SKProductManager.h"
 #import "SynthesizeSingleton.h"
+#import "SKProduct+LocalizedPrice.h"
+
+#define KEY_SKPRODUCT_LIST @"KEY_SKPRODUCT_LIST"
 
 @interface SKProductManager()
 
@@ -22,16 +25,40 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(SKProductManager);
     [super dealloc];
 }
 
+- (id)init
+{
+    if (self = [super init]) {
+        NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+        self.productList = [userDefaults objectForKey:KEY_SKPRODUCT_LIST];
+    }
+    
+    return self;
+}
+
 - (SKProduct *)productWithId:(NSString *)productId
 {
     for (SKProduct *product in _productList) {
-        if (product.productIdentifier == productId) {
+        NSString *productIdentifier = product.productIdentifier;
+        if ([productIdentifier isEqualToString:productId]) {
             return product;
         }
     }
     return nil;
 }
 
+- (void)setProductList:(NSArray *)productList
+{
+    if ([productList count] <= 0) {
+        return;
+    }
+    
+    [_productList release];
+    _productList = [productList retain];
+    
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    [userDefaults setObject:[self.productList data]forKey:KEY_SKPRODUCT_LIST];
+    [userDefaults synchronize];
+}
 
 
 @end
