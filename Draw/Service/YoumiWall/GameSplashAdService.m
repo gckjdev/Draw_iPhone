fcc2030b5f84b59bbcfee2ae768c7ce6c99126e8
Benson Peng <penglingzhe@gmail.com>
2013-05-05 17:48:40 +0800

    update splash ad

--- Draw/Service/YoumiWall/GameSplashAdService.m
+++ Draw/Service/YoumiWall/GameSplashAdService.m
@@ -7,7 +7,18 @@
 //
 
 #import "GameSplashAdService.h"
+#import "AdmobSplashAdService.h"
+#import "DomobSplashAdService.h"
 
 @implementation GameSplashAdService
+
+// only has user, then show splash ad
+// show ad by country and language
+// EN : AdMob, CN : Domob
+// config to disable/enable splash ad
+
+// db.action.update({_id:ObjectId("518551bfe4b0b20c0a7aa4fa")},{$set:{type:5,target_uid:"5122d616e4b098c397bf99bb",target_nick:"击毙客"}})
+
+
 
 @end
