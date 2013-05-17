f508633d02d84da0b5cb63293146093365e165b4
kira <ouyongyong@163.com>
2013-05-17 17:03:39 +0800

    update

--- Draw/Controller/Draw/Offline/OfflineDrawViewController.m
+++ Draw/Controller/Draw/Offline/OfflineDrawViewController.m
@@ -1064,7 +1064,7 @@
     NSString* text = nil;
     
     if ([[self getOpusComment] length] > 0){
-        text = [NSString stringWithFormat:NSLS(@"kShareMyOpusWithDescriptionText"), [self getOpusComment], snsOfficialNick, self.word.text, [ConfigManager getSNSShareSubject], [ConfigManager getDrawAppLink]];
+        text = [NSString stringWithFormat:NSLS(@"kShareMyOpusWithDescriptionText"), [self getOpusComment], snsOfficialNick, self.word.text, [ConfigManager getSNSShareSubject], [UIUtils getAppLink:[ConfigManager appId]]];
     }
     else{
         text = [NSString stringWithFormat:NSLS(@"kShareMyOpusWithoutDescriptionText"), snsOfficialNick, self.word.text, [ConfigManager getSNSShareSubject], [ConfigManager getDrawAppLink]];
