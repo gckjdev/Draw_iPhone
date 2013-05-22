a75a6ae9665e18d59f6c36f0f0c49432dad9bc91
xiaotaowang <445242970@qq.com>
2013-05-22 15:19:01 +0800

    add sing proto

---
+++ Draw/Model/GameApp/SingApp.h
@@ -0,0 +1,14 @@
+//
+//  SingApp.h
+//  Draw
+//
+//  Created by 王 小涛 on 13-5-21.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "GameApp.h"
+
+@interface SingApp : NSObject<GameAppProtocol>
+
+@end
---
+++ Draw/Model/GameApp/SingApp.m
@@ -0,0 +1,13 @@
+//
+//  SingApp.m
+//  Draw
+//
+//  Created by 王 小涛 on 13-5-21.
+//
+//
+
+#import "SingApp.h"
+
+@implementation SingApp
+
+@end
