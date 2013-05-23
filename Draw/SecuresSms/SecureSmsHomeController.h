78fdba238183d32154afe43a88be0efb1a0a0d7f
haodong <qiuhaodong2006@126.com>
2013-05-23 15:30:55 +0800

    update

--- Draw/SecuresSms/SecureSmsHomeController.h
+++ Draw/SecuresSms/SecureSmsHomeController.h
@@ -8,6 +8,23 @@
 
 #import "PPViewController.h"
 
+typedef enum
+{
+    PureChatTypeSecureSms = 1,
+    PureChatTypeCallTrack = 2,
+} PureChatType;
+
 @interface SecureSmsHomeController : PPViewController
+@property (retain, nonatomic) IBOutlet UIButton *chatButton;
+@property (retain, nonatomic) IBOutlet UIButton *friendsButton;
+@property (retain, nonatomic) IBOutlet UIButton *meButton;
+@property (retain, nonatomic) IBOutlet UIButton *supportButton;
+
+- (id)initWithType:(PureChatType)type;
+
+- (IBAction)clickChatButton:(id)sender;
+- (IBAction)clickFriendsButton:(id)sender;
+- (IBAction)clickMeButton:(id)sender;
+- (IBAction)clickSupportButton:(id)sender;
 
 @end
