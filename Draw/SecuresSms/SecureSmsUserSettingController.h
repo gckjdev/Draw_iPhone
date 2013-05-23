537829d48187a3d54441044d7e48185680b1e0f5
haodong <qiuhaodong2006@126.com>
2013-05-23 18:55:20 +0800

    update

---
+++ Draw/SecuresSms/SecureSmsUserSettingController.h
@@ -0,0 +1,13 @@
+//
+//  SecureSmsUserSettingController.h
+//  Draw
+//
+//  Created by haodong on 13-5-23.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecureSmsUserSettingController : PPViewController
+
+@end
