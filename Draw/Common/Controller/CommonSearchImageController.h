b25e9c186265d1b935368bf4c8d4e2afbd09cb06
kira <ouyongyong@163.com>
2013-06-05 15:11:16 +0800

    add search image view

--- Draw/Common/Controller/CommonSearchImageController.h
+++ Draw/Common/Controller/CommonSearchImageController.h
@@ -8,8 +8,10 @@
 
 #import "CommonTabController.h"
 #import "GoogleCustomSearchService.h"
+#import "MWPhotoBrowser.h"
+#import "SearchResultView.h"
 
-@interface CommonSearchImageController : CommonTabController <UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate, GoogleCustomSearchServiceDelegate>
+@interface CommonSearchImageController : CommonTabController <UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate, GoogleCustomSearchServiceDelegate, MWPhotoBrowserDelegate, SearchResultViewDelegate>
 
 @property (retain, nonatomic) IBOutlet UISearchBar* searchBar;
 
