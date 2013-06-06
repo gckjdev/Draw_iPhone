8836041efdb2397bf860e7a9197145105966ffab
haodong <qiuhaodong2006@126.com>
2013-06-06 16:44:48 +0800

    add AskPsApp

---
+++ Draw/Model/GameApp/AskPsApp.h
@@ -0,0 +1,14 @@
+//
+//  AskPsApp.h
+//  Draw
+//
+//  Created by haodong on 13-6-6.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "DrawGameApp.h"
+
+@interface AskPsApp : DrawGameApp
+
+@end
