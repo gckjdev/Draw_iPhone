7bbffca5f5b5923e260b7209fa5cf5a2775c0a3f
haodong <qiuhaodong2006@126.com>
2013-06-14 18:59:07 +0800

    update

---
+++ Draw/AskPs/Controller/AskPsListController.m
@@ -0,0 +1,25 @@
+//
+//  AskPsListController.m
+//  Draw
+//
+//  Created by haodong on 13-6-14.
+//
+//
+
+#import "AskPsListController.h"
+
+@interface AskPsListController ()
+
+@end
+
+@implementation AskPsListController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+
+
+@end
