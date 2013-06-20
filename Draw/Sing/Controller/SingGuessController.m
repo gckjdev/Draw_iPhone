c21bbc0a504a28a8c393d11a38ad8089483cba41
xiaotaowang <445242970@qq.com>
2013-06-20 20:25:13 +0800

    commit

--- Draw/Sing/Controller/SingGuessController.m
+++ Draw/Sing/Controller/SingGuessController.m
@@ -14,13 +14,29 @@
 
 @implementation SingGuessController
 
+- (void)dealloc {
+    [_wordInputView release];
+    [_pbOpus release];
+    [super dealloc];
+}
+
+- (id)initWithPBOpus:(PBOpus *)pbOpus{
+    
+    if (self = [super init]) {
+        
+        self.pbOpus = pbOpus;
+    }
+    
+    return self;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     _wordInputView.answerImage = [UIImage imageNamed:@"wood_button@2x.png"];
     _wordInputView.candidateImage = [UIImage imageNamed:@"wood_button@2x.png"];
-    _wordInputView.answer = @"草泥马";
+    _wordInputView.answer = _pbOpus.name;
     [_wordInputView setCandidates:@"是克拉草建设的离开泥法国就阿拉山马口" column:9];
 }
 
@@ -30,51 +46,11 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)dealloc {
-    [_wordInputView release];
-    [super dealloc];
-}
+
 - (void)viewDidUnload {
+    
     [self setWordInputView:nil];
     [super viewDidUnload];
 }
 
-- (IBAction)clickChangeButton:(id)sender {
-    
-    static BOOL flag;
-    flag = !flag;
-    if (flag) {
-        [_wordInputView setCandidates:@"哈草斯科斯坦共泥和国的天下马无敌的没包含实例变量类别" column:9];
-    }else{
-        [_wordInputView setCandidates:@"是克拉草建设的离开泥法国就阿拉山马口" column:9];
-    }
-}
-
-- (IBAction)clickResetButton:(id)sender {
-
-    
-    [_wordInputView reset];
-}
-
-- (IBAction)clickBombButton:(id)sender {
-    
-    [_wordInputView bomb:3];
-}
-- (IBAction)clickAlignmentButton:(id)sender {
-    static int ali = 0;
-    ali ++;
-    [_wordInputView setAlignment:(ali % 3)];
-}
-
-- (IBAction)clickHGap:(id)sender {
-    static int hgap = 0;
-    hgap += 2;
-    [_wordInputView setHGap:hgap];
-}
-
-- (IBAction)clickWGap:(id)sender {
-    static int hgap = 0;
-    hgap += 2;
-    [_wordInputView setWGap:hgap];
-}
 @end
