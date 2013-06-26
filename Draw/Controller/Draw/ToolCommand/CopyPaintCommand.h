8a46082969c4ba70da3134cf789cbfebd128b9e3
kira <ouyongyong@163.com>
2013-06-26 10:16:44 +0800

    update

---
+++ Draw/Controller/Draw/ToolCommand/CopyPaintCommand.h
@@ -0,0 +1,14 @@
+//
+//  DrawToCommand.h
+//  Draw
+//
+//  Created by gamy on 13-3-25.
+//
+//
+
+#import "ToolCommand.h"
+#import "FriendController.h"
+
+@interface CopyPaintCommand : ToolCommand
+
+@end
---
+++ Draw/Controller/Draw/ToolCommand/CopyPaintCommand.m
@@ -0,0 +1,46 @@
+//
+//  DrawToCommand.m
+//  Draw
+//
+//  Created by gamy on 13-3-25.
+//
+//
+
+#import "CopyPaintCommand.h"
+#import "MyFriend.h"
+#import "UIImageUtil.h"
+
+@implementation CopyPaintCommand
+
+-(void)sendAnalyticsReport{
+//    AnalyticsReport(DRAW_CLICK_CHANGE_DRAWTOUSER);
+}
+
+
+//- (void)changeTargetFriend:(MyFriend *)aFriend
+//{
+//    if (aFriend) {
+//        [self.toolHandler changeDrawToFriend:aFriend];
+//        [self.toolPanel updateDrawToUser:aFriend];
+//    }
+//}
+
+//- (void)friendController:(FriendController *)controller
+//         didSelectFriend:(MyFriend *)aFriend
+//{
+//    [self changeTargetFriend:aFriend];
+//    [controller.navigationController popViewControllerAnimated:YES];
+//}
+
+- (BOOL)execute
+{
+    //TODO enter MyFriendController and select the friend
+//    FriendController *fc = [[FriendController alloc] initWithDelegate:self];
+//    [[[self.toolPanel theViewController] navigationController] pushViewController:fc animated:YES];
+//    [fc release];
+    
+    return YES;
+}
+
+
+@end
