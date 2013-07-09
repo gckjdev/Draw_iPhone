c96940f5e672ec8f739532e78602323dab2bb7e8
GamyHuang <hguangm2009@gmail.com>
2013-07-09 10:08:20 +0800

    add selector box

---
+++ Draw/Controller/Draw/Tool/SelectorBox.h
@@ -0,0 +1,13 @@
+//
+//  SelectorBox.h
+//  Draw
+//
+//  Created by gamy on 13-7-9.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectorBox : UIView
+
+@end
---
+++ Draw/Controller/Draw/Tool/SelectorBox.m
@@ -0,0 +1,31 @@
+//
+//  SelectorBox.m
+//  Draw
+//
+//  Created by gamy on 13-7-9.
+//
+//
+
+#import "SelectorBox.h"
+
+@implementation SelectorBox
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
