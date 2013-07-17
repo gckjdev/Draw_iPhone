5302150a1591732ae7095ed5592eb6b5953a0657
Benson Peng <penglingzhe@gmail.com>
2013-07-17 21:19:16 +0800

    fix server address

--- Draw/DrawAppDelegate.m
+++ Draw/DrawAppDelegate.m
@@ -85,7 +85,7 @@ NSString* GlobalGetServerURL()
 {
 
 #ifdef DEBUG
-    return @"http://localhost:8000/api/i?";
+//    return @"http://localhost:8000/api/i?";
 //    return @"http://58.215.160.100:8002/api/i?";
 //    return @"http://192.168.1.13:8001/api/i?";
 //    return @"http://58.215.160.100:8020/api/i?";
@@ -120,7 +120,7 @@ NSString* GlobalGetTrafficServerURL()
 //    }
 #endif
 #ifdef DEBUG
-    return @"http://localhost:8100/api/i?";
+//    return @"http://localhost:8100/api/i?";
 //    return @"http://58.215.184.18:8699/api/i?";
 
 //    return @"http://58.215.184.18:8037/api/i?";
