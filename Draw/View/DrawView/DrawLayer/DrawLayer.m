697eeb392403668ecf4500a76c1fd657a25bc4bb
GamyHuang <hguangm2009@gmail.com>
2013-07-23 10:42:30 +0800

    add draw layer class to draw project

---
+++ Draw/View/DrawView/DrawLayer/DrawLayer.m
@@ -0,0 +1,19 @@
+//
+//  DrawLayer.m
+//  TestCodePool
+//
+//  Created by gamy on 13-7-22.
+//  Copyright (c) 2013年 orange. All rights reserved.
+//
+
+#import "DrawLayer.h"
+
+@implementation DrawLayer
+
+
+- (void)drawInContext:(CGContextRef)ctx
+{
+    
+}
+
+@end
