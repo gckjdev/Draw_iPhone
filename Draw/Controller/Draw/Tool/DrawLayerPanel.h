d21466cd3a4c0a65d360f3010a4ff3375a8cc01d
GamyHuang <hguangm2009@gmail.com>
2013-08-05 15:06:58 +0800

    add new images

---
+++ Draw/Controller/Draw/Tool/DrawLayerPanel.h
@@ -0,0 +1,17 @@
+//
+//  DrawLayerPanel.h
+//  Draw
+//
+//  Created by gamy on 13-8-5.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrawLayerPanelCell : UITableViewCell
+
+@end
+
+@interface DrawLayerPanel : UIView
+
+@end
---
+++ Draw/Controller/Draw/Tool/DrawLayerPanel.m
@@ -0,0 +1,31 @@
+//
+//  DrawLayerPanel.m
+//  Draw
+//
+//  Created by gamy on 13-8-5.
+//
+//
+
+#import "DrawLayerPanel.h"
+
+@implementation DrawLayerPanel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
