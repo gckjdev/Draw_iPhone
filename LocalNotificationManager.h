ee192723fb2ae97c7b2889538214007de031af60
xiaotaowang <445242970@qq.com>
2013-08-21 14:06:10 +0800

    add

---
+++ LocalNotificationManager.h
@@ -0,0 +1,13 @@
+//
+//  LocalNotificationManager.h
+//  Draw
+//
+//  Created by 王 小涛 on 13-8-21.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocalNotificationManager : NSObject
+
+@end
---
+++ LocalNotificationManager.m
@@ -0,0 +1,13 @@
+//
+//  LocalNotificationManager.m
+//  Draw
+//
+//  Created by 王 小涛 on 13-8-21.
+//
+//
+
+#import "LocalNotificationManager.h"
+
+@implementation LocalNotificationManager
+
+@end
