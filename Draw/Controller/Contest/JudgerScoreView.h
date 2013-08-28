228eb1cc34257c58367cb8268ebe91d87caea5fa
GamyHuang <hguangm2009@gmail.com>
2013-08-28 15:23:03 +0800

    add judger score view

---
+++ Draw/Controller/Contest/JudgerScoreView.h
@@ -0,0 +1,13 @@
+//
+//  JudgerScoreView.h
+//  Draw
+//
+//  Created by old13 on 13-8-28.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JudgerScoreView : UIScrollView
+
+@end
---
+++ Draw/Controller/Contest/JudgerScoreView.m
@@ -0,0 +1,31 @@
+//
+//  JudgerScoreView.m
+//  Draw
+//
+//  Created by old13 on 13-8-28.
+//
+//
+
+#import "JudgerScoreView.h"
+
+@implementation JudgerScoreView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
