1356a9ba6647eee56553e3df0b597a99d6b4ebba
xiaotaowang <445242970@qq.com>
2013-09-09 16:48:49 +0800

    调整猜画详情

--- Draw/DrawAppDelegate.m
+++ Draw/DrawAppDelegate.m
@@ -93,7 +93,7 @@ NSString* GlobalGetServerURL()
 //    return @"http://58.215.160.100:8002/api/i?";
 //    return @"http://192.168.1.13:8001/api/i?";
 //    return @"http://58.215.160.100:8020/api/i?";
-
+    return @"http://192.168.1.198:8000/api/i?";
 //    return @"http://58.215.160.100:8888/api/i?";
 //
     NSUserDefaults* def = [NSUserDefaults standardUserDefaults];
@@ -103,7 +103,6 @@ NSString* GlobalGetServerURL()
         return [NSString stringWithFormat:@"http://%@/api/i?",str];
     }
 
-    return @"http://192.168.1.198:8000/api/i?";
 
 
 #endif
