4d3179ed1f7836bb9913a9bfba43089e2be062c1
xiaotaowang <445242970@qq.com>
2013-09-16 13:44:50 +0800

    update

--- Draw/Common/View/CommonMessageCenter.h
+++ Draw/Common/View/CommonMessageCenter.h
@@ -24,9 +24,13 @@
                   delayTime:(float)delayTime
                isSuccessful:(BOOL)isSuccessful;
 
+- (void)postWithCustomView:(UIView *)view
+                 delayTime:(float)delayTime;
+
+
 @end
 
 #define POSTMSG(msg)    ([[CommonMessageCenter defaultCenter] postMessageWithText:msg delayTime:1.5])
 #define POSTMSG2(msg, sec)    ([[CommonMessageCenter defaultCenter] postMessageWithText:msg delayTime:sec])
 
-
+#define POSTVIEW(v, sec)  ([[CommonMessageCenter defaultCenter] postWithCustomView:v delayTime:sec])
