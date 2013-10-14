e4afbfeec781a5f9a2bba8d2903b711dfaac1f94
GamyHuang <hguangm2009@gmail.com>
2013-10-14 17:35:18 +0800

    update default avatar image && add painter controller

---
+++ Draw/Controller/Feed/Hot/PainterController.m
@@ -0,0 +1,38 @@
+//
+//  PainterController.m
+//  Draw
+//
+//  Created by Gamy on 13-10-14.
+//
+//
+
+#import "PainterController.h"
+
+@interface PainterController ()
+
+@end
+
+@implementation PainterController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
