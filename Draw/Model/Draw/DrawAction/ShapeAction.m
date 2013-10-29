08320205a562087094637d89557ba48f51bbac0d
Benson Peng <penglingzhe@gmail.com>
2013-10-29 09:56:32 +0800

    optimize transparent layer with rect for shadow display in PaintAction and ShapeAction

--- Draw/Model/Draw/DrawAction/ShapeAction.m
+++ Draw/Model/Draw/DrawAction/ShapeAction.m
@@ -50,7 +50,11 @@
     CGContextSetLineJoin(context, kCGLineJoinRound);
 
     if ([self needShowShadow]) {
-        CGContextBeginTransparencyLayer(context, NULL);
+//        CGContextBeginTransparencyLayer(context, NULL);
+        CGRect transparentLayerRect = self.shape.redrawRect;
+        [self.shadow spanRect:&transparentLayerRect];
+        CGContextBeginTransparencyLayerWithRect(context, transparentLayerRect, NULL);
+        
         [self.shadow updateContext:context];
         [self.shape drawInContext:context];
         returnRect = self.shape.redrawRect;
