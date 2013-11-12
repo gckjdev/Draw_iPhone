d58e01880d4f1b7bf90c045a3ca89711ebbb3fd1
Benson Peng <penglingzhe@gmail.com>
2013-11-12 16:08:39 +0800

    update project , add song files into draw project

--- Draw/Model/Opus/OpusDesignTime.h
+++ Draw/Model/Opus/OpusDesignTime.h
@@ -8,6 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum {
+    OPUS_DESIGN_UNKNOWN = 0,
+    OPUS_DESIGN_START = 1,
+    OPUS_DESIGN_PAUSE = 2,
+    OPUS_DESIGN_END = 3,
+} OpusDesignTimeState;
+
 @interface OpusDesignTime : NSObject
+
+@property (nonatomic, assign) OpusDesignTimeState state;
+@property (nonatomic, assign) int beginTime;
+@property (nonatomic, assign) int lastPauseTime;
+@property (nonatomic, assign) int totalTime;
+
+- (id)initWithTime:(int)time;
+- (void)start;
+- (void)pause;
+- (void)stop;
 
 @end
