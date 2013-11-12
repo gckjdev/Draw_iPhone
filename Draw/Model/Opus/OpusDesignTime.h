5719e64ad63cbec9b643cc38573fa9376f373ca6
Benson Peng <penglingzhe@gmail.com>
2013-11-12 14:42:35 +0800

    add opus design time

---
+++ Draw/Model/Opus/OpusDesignTime.h
@@ -0,0 +1,13 @@
+//
+//  OpusDesignTime.h
+//  Draw
+//
+//  Created by qqn_pipi on 13-11-12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpusDesignTime : NSObject
+
+@end
---
+++ Draw/Model/Opus/OpusDesignTime.m
@@ -0,0 +1,13 @@
+//
+//  OpusDesignTime.m
+//  Draw
+//
+//  Created by qqn_pipi on 13-11-12.
+//
+//
+
+#import "OpusDesignTime.h"
+
+@implementation OpusDesignTime
+
+@end
