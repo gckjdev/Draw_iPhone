14120d7daa839ad13ac02529f6f9e5fee87a4978
GamyHuang <hguangm2009@gmail.com>
2013-11-13 15:35:33 +0800

    add new files.

---
+++ Draw/Group/Cell/GroupCell.h
@@ -0,0 +1,13 @@
+//
+//  GroupCell.h
+//  Draw
+//
+//  Created by Gamy on 13-11-13.
+//
+//
+
+#import "PPTableViewCell.h"
+
+@interface GroupCell : PPTableViewCell
+
+@end
---
+++ Draw/Group/Controller/GroupDetailController.h
@@ -0,0 +1,13 @@
+//
+//  GroupDetailController.h
+//  Draw
+//
+//  Created by Gamy on 13-11-13.
+//
+//
+
+#import "PPTableViewController.h"
+
+@interface GroupDetailController : PPTableViewController
+
+@end
