06f0a14ff3b0af394f0896fcd8b4193f57cf074a
xiaotaowang <445242970@qq.com>
2013-11-18 22:06:58 +0800

    commit

--- Draw/Sing/View/WhisperStyleView.m
+++ Draw/Sing/View/WhisperStyleView.m
@@ -84,7 +84,7 @@ AUTO_CREATE_VIEW_BY_XIB(WhisperStyleView);
             l.textOutlineColor = [[DrawColor colorWithBetterCompressColor:labelInfo.textStrokeColor] color];
             
             // set text stroke widht
-            l.textOutlineWidth = (ISIPAD ? 2 : 1);
+            l.textOutlineWidth = (ISIPAD ? 4 : 2);
                         
         }else{
             
