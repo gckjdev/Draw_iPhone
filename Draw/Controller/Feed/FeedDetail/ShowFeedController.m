c6d88281ba3bb4c8bb7171a5e1a29757c27b4a0e
xiaotaowang <445242970@qq.com>
2013-11-21 17:23:41 +0800

    增加音频后台播放功能

--- Draw/Controller/Feed/FeedDetail/ShowFeedController.m
+++ Draw/Controller/Feed/FeedDetail/ShowFeedController.m
@@ -55,6 +55,7 @@
 #import "AudioPlayer.h"
 #import "GameSNSService.h"
 #import "ShareAction.h"
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface ShowFeedController ()<OpusImageBrowerDelegate> {
     BOOL _didLoadDrawPicture;
@@ -531,6 +532,23 @@ typedef enum{
     [self updateActionButtons];
     [self.feed setDrawData:nil];
     [self.feed setPbDrawData:nil];
+    
+    if ([self.feed isSingCategory]) {
+        [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
+        [self becomeFirstResponder];
+        [self configNowPlayingInfoCenter];
+    }
+}
+
+
+-(void)viewWillDisappear:(BOOL)animated{
+    
+    [super viewWillDisappear:animated];
+    
+    if ([self.feed isSingCategory]) {
+        [[UIApplication sharedApplication] endReceivingRemoteControlEvents];
+        [self resignFirstResponder];
+    }
 }
 
 - (void)viewDidDisappear:(BOOL)animated
@@ -928,9 +946,10 @@ typedef enum{
 //override super clickBlackButton method
 - (IBAction)clickBackButton:(id)sender
 {
-    [_audioPlayer stop];
-    [self.feedScene didClickBackBtn:self];
     
+    [_audioPlayer stop];
+    
+    [self.feedScene didClickBackBtn:self];
     // clear delegate to avoid callback
     self.drawCell.delegate = nil;
     self.drawCellFullScreen.delegate = nil;
@@ -1058,7 +1077,7 @@ typedef enum{
 - (void)viewDidLoad
 {
     [self baseInit];
-    if (isSingApp()) {
+    if ([self.feed isSingCategory]) {
         self.canDragBack = NO;
     }
 }
@@ -1401,6 +1420,49 @@ typedef enum{
 - (void)sliderValueChange:(CustomSlider *)slider{
     
     [_audioPlayer seekToProgress:slider.value];
+}
+
+-(void)configNowPlayingInfoCenter{
+    
+    NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
+    [dict setObject:self.feed.wordText forKey:MPMediaItemPropertyTitle];
+    [dict setObject:self.feed.author.nickName forKey:MPMediaItemPropertyArtist];
+    
+    UIImage *image = [[SDImageCache sharedImageCache] imageFromMemoryCacheForKey:self.feed.pbFeed.opusImage];
+    if (image == nil) {
+        image = [[SDImageCache sharedImageCache] imageFromMemoryCacheForKey:self.feed.pbFeed.opusThumbImage];
+    }
+    
+    if (image != nil) {
+        MPMediaItemArtwork *artwork = [[MPMediaItemArtwork alloc] initWithImage:image];
+        [dict setObject:artwork forKey:MPMediaItemPropertyArtwork];
+    }
+    
+    [[MPNowPlayingInfoCenter defaultCenter] setNowPlayingInfo:dict];
+}
+
+
+
+-(BOOL)canBecomeFirstResponder{
+    return YES;
+}
+
+-(void)remoteControlReceivedWithEvent:(UIEvent *)event{
+    
+    //if it is a remote control event handle it correctly
+    if (event.type == UIEventTypeRemoteControl) {
+        switch (event.subtype) {
+            case UIEventSubtypeRemoteControlTogglePlayPause:
+            {
+                [_audioPlayer play];
+                break;
+            }
+            
+            default:
+                break;
+        }
+    }
+    
 }
 
 @end
