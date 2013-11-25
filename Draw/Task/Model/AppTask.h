cce5e8ea585e28803f5128a3ac32f7297783b658
Benson Peng <penglingzhe@gmail.com>
2013-11-25 14:50:58 +0800

    add app task manager

---
+++ Draw/Task/Model/AppTask.h
@@ -0,0 +1,13 @@
+//
+//  AppTask.h
+//  Draw
+//
+//  Created by qqn_pipi on 13-11-25.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppTask : NSObject
+
+@end
---
+++ Draw/Task/Model/AppTask.m
@@ -0,0 +1,13 @@
+//
+//  AppTask.m
+//  Draw
+//
+//  Created by qqn_pipi on 13-11-25.
+//
+//
+
+#import "AppTask.h"
+
+@implementation AppTask
+
+@end
