4b152e5aae171634257a08eadb5fd0c7fe6a4ac5
Benson Peng <penglingzhe@gmail.com>
2014-01-24 13:26:53 +0800

    add vip controller and images

---
+++ Draw/Controller/User/PurchaseVipController.h
@@ -0,0 +1,13 @@
+//
+//  PurchaseVipController.h
+//  Draw
+//
+//  Created by qqn_pipi on 14-1-24.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PurchaseVipController : UIViewController
+
+@end
---
+++ Draw/Controller/User/PurchaseVipController.m
@@ -0,0 +1,38 @@
+//
+//  PurchaseVipController.m
+//  Draw
+//
+//  Created by qqn_pipi on 14-1-24.
+//
+//
+
+#import "PurchaseVipController.h"
+
+@interface PurchaseVipController ()
+
+@end
+
+@implementation PurchaseVipController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
