d4e73c2b5b1f20287893a4a7c0e50caa6156293d
xiaotaowang <445242970@qq.com>
2014-02-10 17:40:18 +0800

    适配任务新界面

--- Draw/Task/Controller/TaskCell.m
+++ Draw/Task/Controller/TaskCell.m
@@ -10,6 +10,8 @@
 #import "GameTask.h"
 
 @interface TaskCell ()
+@property (retain, nonatomic) IBOutlet UIImageView *bgImageView;
+@property (retain, nonatomic) IBOutlet UIImageView *doneImageView;
 
 @end
 
@@ -17,7 +19,7 @@
 
 + (float)getCellHeight{
     
-    return CELL_CONST_HEIGHT;
+    return ISIPAD ? 153 : 70;
 }
 
 + (NSString *)getCellIdentifier{    
@@ -27,6 +29,7 @@
 
 - (void)setCellInfo:(GameTask*)task
 {
+    self.backgroundColor = [UIColor clearColor];
     [_taskNameLabel setFont:CELL_CONTENT_FONT];
     [_taskDescLabel setFont:CELL_SMALLTEXT_FONT];
     [_taskStatusLabel setFont:CELL_CONTENT_FONT];
@@ -37,11 +40,17 @@
     
     _taskDescLabel.text = task.desc;
     _taskNameLabel.text = task.name;
+    
+    _bgImageView.layer.borderColor = COLOR_YELLOW.CGColor;
+    _bgImageView.layer.borderWidth = (ISIPAD ? 4 : 2);
+    _bgImageView.layer.cornerRadius = (ISIPAD ? 16 : 8);
+    _bgImageView.clipsToBounds = YES;
 
     SET_BUTTON_ROUND_STYLE_ORANGE(_awardButton);
     [_awardButton setTitle:NSLS(@"kTakeAward") forState:UIControlStateNormal];
     [_awardButton.titleLabel setFont:CELL_CONTENT_FONT];
     
+    _doneImageView.hidden = YES;
     switch (task.status){
         case PBTaskStatusTaskStatusAlwaysOpen:
             [_badgeView setNumber:task.badge];
@@ -63,7 +72,9 @@
             
         case PBTaskStatusTaskStatusAward:
             [_badgeView setNumber:0];
-            _taskStatusLabel.text = NSLS(@"kTaskTake");
+//            _taskStatusLabel.text = NSLS(@"kTaskTake");
+            _taskStatusLabel.text = @"";
+            _doneImageView.hidden = NO;
             _awardButton.hidden = YES;
             break;
                         
@@ -100,6 +111,8 @@
     [_taskDescLabel release];
     [_taskStatusLabel release];
     [_badgeView release];
+    [_bgImageView release];
+    [_doneImageView release];
     [super dealloc];
 }
 
