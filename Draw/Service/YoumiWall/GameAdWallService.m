51ed1e11ee78fbd6a50dea864fed6d2338627a35
Benson Peng <penglingzhe@gmail.com>
2014-02-13 23:47:05 +0800

    add show loading for wall

--- Draw/Service/YoumiWall/GameAdWallService.m
+++ Draw/Service/YoumiWall/GameAdWallService.m
@@ -245,6 +245,7 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(GameAdWallService)
     if ([userId length] == 0)
         return;
     
+    POSTMSG2(@"加载积分墙中...请耐心等待", 3);
     [[self wallServiceByType:wallType forceShowWall:forceShowWall] show:superController userId:userId];
 }
 
