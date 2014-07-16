e00a63e36e3c94382abaaa5af43cbcbab47210e1
ChaoSo <god_super@163.com>
2014-07-16 12:50:23 +0800

    more view root

---
+++ Draw/Home/More/MoreViewCell.h
@@ -0,0 +1,23 @@
+//
+//  MoreViewCell.h
+//  Draw
+//
+//  Created by ChaoSo on 14-7-14.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "StableView.h"
+#import "MoreViewController.h"
+
+@interface MoreViewCell : UICollectionViewCell
+@property (retain, nonatomic) IBOutlet BadgeView *badgeView;
+@property (retain, nonatomic) IBOutlet UIButton *itemButton;
+@property (assign, nonatomic) int currentRow;
+@property (assign, nonatomic) MoreViewController* controller;
+
+
+-(void)updateMoreCollectionCell:(NSInteger)row;
+-(IBAction)clickButton:(id)sender;
+
+@end
