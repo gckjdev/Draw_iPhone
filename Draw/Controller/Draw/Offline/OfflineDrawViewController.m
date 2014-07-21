51f99b1a1dffbcb097fbc08c24b77595795037ca
Charlie Huang <hyichao@foxmail.com>
2014-07-21 11:30:30 +0800

    modify score engine

--- Draw/Controller/Draw/Offline/OfflineDrawViewController.m
+++ Draw/Controller/Draw/Offline/OfflineDrawViewController.m
@@ -1637,11 +1637,13 @@
     // 评分
     NSString* sourcePath = [self writeImageToFile:_copyView.image filePath:self.draft.draftId];
     NSString* destPath = self.tempImageFilePath;
-    [ImageSimilarityEngine score1SrcPath:sourcePath destPath:destPath];
 
-    int score = [OpenCVUtils simpleDrawScoreSourceImagePath:sourcePath destImagePath:destPath];
+    //从关卡传入difficulty
+    int score = [ImageSimilarityEngine score1SrcPath:sourcePath destPath:destPath difficulty:1.0];
     
-    score = [OpenCVUtils hausdorffScoreSourceImagePath:sourcePath destImagePath:destPath];
+
+//    int score = [OpenCVUtils simpleDrawScoreSourceImagePath:sourcePath destImagePath:destPath];
+//    score = [OpenCVUtils hausdorffScoreSourceImagePath:sourcePath destImagePath:destPath];
 //    score = [OpenCVUtils cosineScoreSourceImagePath:sourcePath destImagePath:destPath];
 //    [OpenCVUtils testSourceImagePath:sourcePath destImagePath:destPath];
     
