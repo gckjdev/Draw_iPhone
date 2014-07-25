09917ce011ce3969b21f4a789e228219430610a7
ChaoSo <god_super@163.com>
2014-07-25 17:41:47 +0800

    tip page

--- Draw/Common/View/CommonDialog.h
+++ Draw/Common/View/CommonDialog.h
@@ -17,6 +17,7 @@ typedef enum {
     CommonDialogStyleDoubleButtonWithCross,
     CommonDialogStyleCross,
     CommonDialogStyleSingleButtonWithCross,
+    CommonSquareDialogStyleCross,
 }CommonDialogStyle;
 
 typedef enum {
---
+++ Draw/Tutorial/Controller/TipsPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipsPageViewController.h
+//  Draw
+//
+//  Created by ChaoSo on 14-7-25.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "SGFocusImageFrame.h"
+@interface TipsPageViewController : UIViewController<SGFocusImageFrameDelegate>
+
+@end
