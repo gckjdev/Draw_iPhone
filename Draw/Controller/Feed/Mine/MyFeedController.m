02940c3a22cd904a1251564414a90e9214022336
ChaoSo <god_super@163.com>
2014-07-26 09:24:34 +0800

    client getFeedList api

--- Draw/Controller/Feed/Mine/MyFeedController.m
+++ Draw/Controller/Feed/Mine/MyFeedController.m
@@ -482,7 +482,7 @@ typedef enum{
 
 - (NSInteger)tabCount
 {
-    return 4;
+    return 5;
 }
 - (NSInteger)fetchDataLimitForTabIndex:(NSInteger)index
 {
@@ -490,21 +490,21 @@ typedef enum{
 }
 - (NSInteger)tabIDforIndex:(NSInteger)index
 {
-    NSInteger tabId[] = {MyTypeTimelineOpus,MyTypeTimelineGuess,MyTypeComment,MyTypeDrawToMe};
+    NSInteger tabId[] = {MyTypeTimelineOpus,MyTypeTimelineGuess,MyTypeComment,MyTypeDrawToMe,MyTypeTimeLineDraw};
 
     return tabId[index];
 }
 
 - (NSString *)tabNoDataTipsforIndex:(NSInteger)index
 {
-    NSString *tabDesc[] = {NSLS(@"kNoMyFeed"),NSLS(@"kNoTimelineGuess"),NSLS(@"kNoMyComment"),NSLS(@"kNoDrawToMe")};
+    NSString *tabDesc[] = {NSLS(@"kNoMyFeed"),NSLS(@"kNoTimelineGuess"),NSLS(@"kNoMyComment"),NSLS(@"kNoDrawToMe"),NSLS(@"kNoTimelineDraw")};
     
     return tabDesc[index];
 }
 
 - (NSString *)tabTitleforIndex:(NSInteger)index
 {
-    NSString *tabTitle[] = {NSLS(@"kUserFeed"),NSLS(@"kUserGuess"),NSLS(@"kComment"),NSLS(@"kDrawToMe")};
+    NSString *tabTitle[] = {NSLS(@"kUserFeed"),NSLS(@"kUserGuess"),NSLS(@"kComment"),NSLS(@"kDrawToMe"),NSLS(@"kNoTimelineDraw")};
 
     return tabTitle[index];
 }
@@ -551,8 +551,19 @@ typedef enum{
                 
             {
                 [feedService getMyCommentList:offset limit:limit delegate:self];
-            }
+            
                 break;
+            }
+            case MyTypeTimeLineDraw:
+            {
+                NSString *userId = [[UserManager defaultManager] userId];
+                [feedService getUserOpusList:userId
+                                      offset:offset
+                                       limit:limit
+                                        type:FeedListTypeTimeLineConquerDraw
+                                    delegate:self];
+                break;
+            }
             default:
                 
                 [self hideActivity];
