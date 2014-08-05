3c8133a7531b9e860674fcd72ad8ea33af1d2664
Charlie Huang <hyichao@foxmail.com>
2014-08-05 16:54:45 +0800

    checkout all the incorrect localizable strings, checkout the relavant codes

--- Draw/Tutorial/Model/TutorialCoreManager.m
+++ Draw/Tutorial/Model/TutorialCoreManager.m
@@ -140,12 +140,8 @@ static TutorialCoreManager* _defaultTutorialCoreManager;
     
     //optional
     [tb setCnName:name];
-    [tb setEnName:name];
-    [tb setTcnName:name];
     
     [tb setCnDesc:desc];
-    [tb setEnDesc:desc];
-    [tb setTcnDesc:desc];
     
     [tb setThumbImage:imageUrl];
     [tb setImage:imageUrl];
@@ -351,9 +347,10 @@ static TutorialCoreManager* _defaultTutorialCoreManager;
     // TODO add test tutorials
 
     
-    NSArray* testTutorialName = @[NSLS(@"kTutorialNameCubic"),
-                                /*@"kTutorialNameCrazyLines",@"kTutorialNameColorFilling",@"kTutorialNameDoggyDoggy",@"kTutorialNameMoveMoveMove",@"kTutorialNameFaceChanging",*/NSLS(@"kTutorialNameCuteLittleGee"),NSLS(@"kTutorialNameLoveLifeLoveSport"),NSLS(@"kTutorialNameFamousBuilding"),NSLS(@"kTutorialNameMoveMoveMove")];
-    NSArray* testTutorialDesc = @[NSLS(@"kTutorialDescriptionCubic"),/*@"kTutorialDescriptionCrazyLines",@"kTutorialDescriptionColorFilling",@"kTutorialDescriptionDoggyDoggy",@"kTutorialDescriptionMoveMoveMove",@"kTutorialDescriptionFaceChange",*/NSLS(@"kTutorialDescriptionCuteLittleGee"),NSLS(@"kTutorialDescriptionLoveLifeLoveSport"),NSLS(@"kTutorialDescriptionFamousBuilding"),NSLS(@"kTutorialDescriptionMoveMoveMove")];
+    NSArray* testTutorialName = @[@"初识三次元",
+                                /*@"疯狂的线条",@"识色配图",@"旺星人大集合",@"易容速成",*/@"百态小吉萌翻天",@"爱生活爱运动",@"名建筑大集合",@"一起来运动"];
+    
+    NSArray* testTutorialDesc = @[@"教你;怎么把物体从二次元转换成三次元",/*@"帮助你更好的熟练工具",@"这里将把你打造成用色达人",@"属于旺星人的趴",@"你想易容成什么人",*/@"小吉闪亮登场，速来围观",@"童鞋们，一起来运动吧",@"你认识这些建筑吗？",@"燃烧你的卡路里吧"];
 
     
     NSArray* tutorialImageUrl = @[@"http://58.215.160.100:8080/app_res/tutorial/image/title-0.png",
