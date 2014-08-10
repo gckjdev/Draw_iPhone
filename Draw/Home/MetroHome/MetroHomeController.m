2e3a60dd1d5399bbc8392a2d8f0ff0ff7cab38cb
ChaoSo <god_super@163.com>
2014-08-10 15:11:37 +0800

    update

--- Draw/Home/MetroHome/MetroHomeController.m
+++ Draw/Home/MetroHome/MetroHomeController.m
@@ -391,6 +391,7 @@
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
         
         NSMutableArray *itemList = [[[NSMutableArray alloc] init] autorelease];
+      
         for(Billboard *bb in _bbList){
 
             UIImage *image = [bbManager getImage:bb];
@@ -405,6 +406,7 @@
             
             [itemList addObject:item];            
         }
+      
         
         dispatch_async(dispatch_get_main_queue(), ^{
             //新建滚动展览
@@ -416,6 +418,15 @@
             [self.galleryView addSubview:imageFrame];
             [self.galleryView bringSubviewToFront:imageFrame];
             [imageFrame release];
+            
+            if([_bbList count]<=0){
+                UIImageView *image = [[UIImageView alloc]initWithFrame:frame];
+                [image setImage:[UIImage imageNamed:DEFAULT_GALLERY_IMAGE]];
+                [self.galleryView addSubview:image];
+                [self.galleryView bringSubviewToFront:image];
+                [image release];
+                
+            }
         });
     });
     
