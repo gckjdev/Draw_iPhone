7fc0793ae1afcbfe41bca31770eefe16f2fbeedf
Charlie Huang <hyichao@foxmail.com>
2014-09-03 15:55:27 +0800

    update blur brush, update crayon brush

--- Draw/Model/Draw/BrushEffect/OilBrush.m
+++ Draw/Model/Draw/BrushEffect/OilBrush.m
@@ -26,10 +26,29 @@ static dispatch_once_t sharedOilBrushOnceToken;
 
 - (UIImage*)brushImage:(UIColor *)color
 {
-    UIImage* brushImage = [UIImage imageNamed:@"brush_oil2.png"];
-    UIImage *tinted = [brushImage rt_tintedImageWithColor:color
-                                                    level:1.0f];
-    brushImage = tinted;
+//size暂时用20，考虑传入参数 TODO
+    
+    //在context上画出一个渐变园
+    UIImage *brushImage;
+    UIGraphicsBeginImageContext(CGSizeMake(20, 20));
+	CGContextRef ctx = UIGraphicsGetCurrentContext();
+	CGFloat wd = 20 / 2.0f;
+	CGPoint pt = CGPointMake(wd, wd);
+	CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
+	size_t num_locations = 2;
+	CGFloat locations[2] = { 1.0, 0.0 };
+	CGFloat* comp = (CGFloat *)CGColorGetComponents(color.CGColor);
+	CGFloat fc = sinf(((0.5/*这个参数是硬度，暂时固定*/ / 5.0f) * M_PI ) / 2.0f);
+	CGFloat colors[8] = { comp[0], comp[1], comp[2], 0.0f, comp[0], comp[1], comp[2], fc };
+	CGGradientRef gradient = CGGradientCreateWithColorComponents(colorspace, colors, locations, num_locations);
+	CGContextDrawRadialGradient(ctx, gradient, pt, 0.0f, pt, wd, 0);
+	CGContextFlush(ctx);
+	brushImage = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+	CFRelease(gradient);
+	CFRelease(colorspace);
+    
+    //以该渐变园作为brushImage
     return brushImage;
 }
 
