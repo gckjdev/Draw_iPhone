4c753f106dc7dae96591f9aac5967297862fb793
Benson Peng <penglingzhe@gmail.com>
2015-10-05 23:11:33 +0800

    update

--- build_game_protocol.sh
+++ build_game_protocol.sh
@@ -64,31 +64,31 @@ echo build Java server codes
 
 cd /java/protobuf-2.4.1/bin
 
-protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/GameBasic.proto
+protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/DrawJava/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/GameBasic.proto
 
-protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/GameConstants.proto
+protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/DrawJava/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/GameConstants.proto
 
-protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/GameMessage.proto
+protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/DrawJava/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/GameMessage.proto
 
-protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Dice.proto
+protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/DrawJava/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Dice.proto
 
-protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Draw.proto
+protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/DrawJava/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Draw.proto
 
-protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Tutorial.proto
+protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/DrawJava/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Tutorial.proto
 
-protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Group.proto
+protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/DrawJava/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Group.proto
 
-protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ZhaJinHua.proto
+protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/DrawJava/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ZhaJinHua.proto
 
-protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/BBS.proto
+protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/DrawJava/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/BBS.proto
 
-protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Config.proto
+protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/DrawJava/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Config.proto
 
-protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Photo.proto
+protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/DrawJava/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Photo.proto
 
-protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Opus.proto
+protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/DrawJava/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Opus.proto
 
-protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Sing.proto
+protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/DrawJava/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Sing.proto
 
 
 echo build Java Android code
@@ -103,9 +103,9 @@ protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuff
 
 protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Draw_Android/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Draw.proto
 
-protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Tutorial.proto
+protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/DrawJava/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Tutorial.proto
 
-protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Group.proto
+protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/DrawJava/Common_Java_Game/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/Group.proto
 
 protoc --proto_path=/Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ --java_out=/Users/pipi/gitdata/Draw_Android/src/ /Users/pipi/gitdata/Draw_iPhone/Draw/GameServer/ProtocolBuffer/ZhaJinHua.proto
 
