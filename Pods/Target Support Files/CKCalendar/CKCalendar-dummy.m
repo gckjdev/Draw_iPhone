d6662b3c28cd6072ec4974106ad08ebe0dff644b
gckjdev <29356050@qq.com>
2020-04-07 00:00:40 +0800

    add pod files

---
+++ Pods/Target Support Files/CKCalendar/CKCalendar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CKCalendar : NSObject
+@end
+@implementation PodsDummy_CKCalendar
+@end
---
+++ Pods/Target Support Files/Pods-Draw/Pods-Draw-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Draw : NSObject
+@end
+@implementation PodsDummy_Pods_Draw
+@end
